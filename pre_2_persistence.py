b7cfa9102bb67b344f7f04ff619a33665259e164
Peter <peterpfleiderer@gmx.de>
2017-11-14 09:40:54 +0100

    interactive persistence

--- pre_2_persistence.py
+++ pre_2_persistence.py
@@ -13,62 +13,63 @@ model_dict={'MIROC':{'gird':'128x256','path':'/global/cscratch1/sd/pepflei/MIROC
 			'CAM4-2degree':{'gird':'96x144','path':'/global/cscratch1/sd/pepflei/ETH/CAM4-2degree/'},
 }
 
-for model in model_dict.keys():
-	working_path=model_dict[key]['path']
-	grid=model_dict[key]['grid']
+model=sys.argv[1]
 
-	for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
-		run_count=0
-		all_files=glob.glob(working_path+scenario+'/*')
-		for in_file in all_files:
-			test=da.read_nc(in_file)
-			if len(test.time/365.)>9.:
+working_path=model_dict[model]['path']
+grid=model_dict[model]['grid']
+
+for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
+	run_count=0
+	all_files=glob.glob(working_path+scenario+'/*')
+	for in_file in all_files:
+		test=da.read_nc(in_file)
+		if len(test.time/365.)>9.:
+			print in_file
+			if in_file.split('_')[-1]!='period.nc' and run_count<100:
 				print in_file
-				if in_file.split('_')[-1]!='period.nc' and run_count<100:
-					print in_file
-					start_time=time.time()
+				start_time=time.time()
 
-					try:
-						# mask ocean
-						land_file=in_file.replace('.nc','_land.nc')
-						os.system('cdo -O mul '+in_file+' /global/homes/p/pepflei/masks/landmask_'+grid+'_NA-1.nc '+land_file)
+				try:
+					# mask ocean
+					land_file=in_file.replace('.nc','_land.nc')
+					os.system('cdo -O mul '+in_file+' /global/homes/p/pepflei/masks/landmask_'+grid+'_NA-1.nc '+land_file)
 
-						# detrend
-						a=in_file.replace('.nc','_a.nc')
-						b=in_file.replace('.nc','_b.nc')
-						os.system('cdo -O trend '+land_file+' '+a+' '+b)
-						detrend_1=in_file.replace('.nc','_detrend_1.nc')
-						os.system('cdo -O subtrend '+land_file+' '+a+' '+b+' '+detrend_1)
+					# detrend
+					a=in_file.replace('.nc','_a.nc')
+					b=in_file.replace('.nc','_b.nc')
+					os.system('cdo -O trend '+land_file+' '+a+' '+b)
+					detrend_1=in_file.replace('.nc','_detrend_1.nc')
+					os.system('cdo -O subtrend '+land_file+' '+a+' '+b+' '+detrend_1)
 
-						runmean=in_file.replace('.nc','_runmean.nc')
-						os.system('cdo -O runmean,90 '+detrend_1+' '+runmean)
+					runmean=in_file.replace('.nc','_runmean.nc')
+					os.system('cdo -O runmean,90 '+detrend_1+' '+runmean)
 
-						detrend_cut=in_file.replace('.nc','_detrend_cut.nc')
-						command='cdo -O delete,timestep='
-						for i in range(1,46,1): command+=str(i)+','
-						for i in range(1,46,1): command+=str(-i)+','
-						os.system(command+' '+detrend_1+' '+detrend_cut)
-						anom_file=in_file.replace('.nc','_anom.nc')
-						print 'anom',anom_file
-						os.system('cdo -O sub '+detrend_cut+' '+runmean+' '+anom_file)
+					detrend_cut=in_file.replace('.nc','_detrend_cut.nc')
+					command='cdo -O delete,timestep='
+					for i in range(1,46,1): command+=str(i)+','
+					for i in range(1,46,1): command+=str(-i)+','
+					os.system(command+' '+detrend_1+' '+detrend_cut)
+					anom_file=in_file.replace('.nc','_anom.nc')
+					print 'anom',anom_file
+					os.system('cdo -O sub '+detrend_cut+' '+runmean+' '+anom_file)
 
-						# state
-						state_file=in_file.replace('.nc','_state.nc')
-						print 'state',state_file
-						temp_anomaly_to_ind(anom_file,state_file,overwrite=True)
+					# state
+					state_file=in_file.replace('.nc','_state.nc')
+					print 'state',state_file
+					temp_anomaly_to_ind(anom_file,state_file,overwrite=True)
 
-						# persistence
-						print state_file,in_file.replace('.nc','_period.nc')
-						get_persistence(state_file,in_file.replace('.nc','_period.nc'),overwrite=True)
-						print 'processing time:',time.time()-start_time
+					# persistence
+					print state_file,in_file.replace('.nc','_period.nc')
+					get_persistence(state_file,in_file.replace('.nc','_period.nc'),overwrite=True)
+					print 'processing time:',time.time()-start_time
 
-						run_count+=1
+					run_count+=1
 
-					except:
-						failed_files=open('out/step_5_period_fails.txt','w')
-						failed_files.write(state_file+'\n')
-						failed_files.close()
-						print '/!\---------------/!\ \n failed for',state_file,'\n/!\---------------/!\ '
+				except:
+					failed_files=open('out/step_5_period_fails.txt','w')
+					failed_files.write(state_file+'\n')
+					failed_files.close()
+					print '/!\---------------/!\ \n failed for',state_file,'\n/!\---------------/!\ '
 
-					# clean
-					os.system('rm '+land_file+' '+a+' '+b+' '+detrend_1+' '+runmean+' '+detrend_cut+' '+anom_file+' '+state_file)
+				# clean
+				os.system('rm '+land_file+' '+a+' '+b+' '+detrend_1+' '+runmean+' '+detrend_cut+' '+anom_file+' '+state_file)
