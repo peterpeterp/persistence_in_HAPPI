a56da0c411efb0a315dab4f963cdcb4a2abce182
Peter <peterpfleiderer@gmx.de>
2017-11-17 10:33:42 +0100

    overwrite more elegant

--- ana_4_summer_periods.py
+++ ana_4_summer_periods.py
@@ -16,7 +16,7 @@ model_dict={'MIROC':{'grid':'128x256','path':'/global/cscratch1/sd/pepflei/MIROC
 model=sys.argv[1]
 print model
 
-overwrite=True
+overwrite=False
 
 working_path=model_dict[model]['path']
 grid=model_dict[model]['grid']
@@ -25,59 +25,61 @@ for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
     run_count=0
     all_files=glob.glob(working_path+scenario+'/*_period*')
     for in_file in all_files:
-        print in_file
-        tas=da.read_nc(in_file.replace('_period',''))['tas']
-        tt=np.asarray(tas.squeeze(),np.float)
-        datevar = num2date(tas.time,units = "days since 1979-01-01 00:00:00",calendar = "proleptic_gregorian")
-        year=np.array([int(str(date).split("-")[0])	for date in datevar[:]],np.int32)
+        if overwrite and os.path.isfile(out_file):  os.system('rm '+out_file)
+        if os.path.isfile(out_file)==False: 
+            print in_file
+            tas=da.read_nc(in_file.replace('_period',''))['tas']
+            tt=np.asarray(tas.squeeze(),np.float)
+            datevar = num2date(tas.time,units = "days since 1979-01-01 00:00:00",calendar = "proleptic_gregorian")
+            year=np.array([int(str(date).split("-")[0])	for date in datevar[:]],np.int32)
 
-        period=da.read_nc(in_file)
-        mm=np.asarray(period['period_midpoints']-tas.time[0],np.int32)
-        ll=np.asarray(period['period_length'],np.int32)
-        seas=np.asarray(period['period_season'],np.int32)
-        state=np.asarray(period['period_state'],np.int32)
+            period=da.read_nc(in_file)
+            mm=np.asarray(period['period_midpoints']-tas.time[0],np.int32)
+            ll=np.asarray(period['period_length'],np.int32)
+            seas=np.asarray(period['period_season'],np.int32)
+            state=np.asarray(period['period_state'],np.int32)
 
-        time0=time.time()
-        cum_heat,hot_shift,hot_temp,tasX,Ni_new,original_period_id=summer_period_analysis(ll,mm,seas,state,tt,year,len(period.lat),len(period.lon),len(period.period_id))
-        print time.time()-time0
+            time0=time.time()
+            cum_heat,hot_shift,hot_temp,tasX,Ni_new,original_period_id=summer_period_analysis(ll,mm,seas,state,tt,year,len(period.lat),len(period.lon),len(period.period_id))
+            print time.time()-time0
 
-        out_file=in_file.replace('_period','_summer')
-        if overwrite and os.path.isfile(out_file): os.system('rm '+out_file)
-        nc_out=Dataset(out_file,'w')
-        nc_in=Dataset(in_file,'r')
-        for dname, the_dim in nc_in.dimensions.iteritems():
-            if dname in ['lon','lat']:nc_out.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
-        nc_out.createDimension('ID', Ni_new)
+            out_file=in_file.replace('_period','_summer')
 
-        for v_name, varin in nc_in.variables.iteritems():
-            if v_name in ['lon','lat']:
-                outVar = nc_out.createVariable(v_name, varin.datatype, varin.dimensions)
-                outVar.setncatts({k: varin.getncattr(k) for k in varin.ncattrs()})
-                outVar[:] = varin[:]
+            nc_out=Dataset(out_file,'w')
+            nc_in=Dataset(in_file,'r')
+            for dname, the_dim in nc_in.dimensions.iteritems():
+                if dname in ['lon','lat']:nc_out.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
+            nc_out.createDimension('ID', Ni_new)
 
-        outVar = nc_out.createVariable('cumulated_heat','f',('ID','lat','lon',))
-        outVar.long_name='sum over temperature of days in period'
-        outVar[:] = cum_heat[:,:,:]
+            for v_name, varin in nc_in.variables.iteritems():
+                if v_name in ['lon','lat']:
+                    outVar = nc_out.createVariable(v_name, varin.datatype, varin.dimensions)
+                    outVar.setncatts({k: varin.getncattr(k) for k in varin.ncattrs()})
+                    outVar[:] = varin[:]
 
-        outVar = nc_out.createVariable('hottest_day_shift','i',('ID','lat','lon',))
-        outVar.long_name='shift between hottest day in period and middle of period'
-        outVar[:] = hot_shift[:,:,:]
+            outVar = nc_out.createVariable('cumulated_heat','f',('ID','lat','lon',))
+            outVar.long_name='sum over temperature of days in period'
+            outVar[:] = cum_heat[:,:,:]
 
-        outVar = nc_out.createVariable('hottest_day_temp','f',('ID','lat','lon',))
-        outVar.long_name='temperature of hottest day in period'
-        outVar[:] = hot_temp[:,:,:]
+            outVar = nc_out.createVariable('hottest_day_shift','i',('ID','lat','lon',))
+            outVar.long_name='shift between hottest day in period and middle of period'
+            outVar[:] = hot_shift[:,:,:]
 
-        outVar = nc_out.createVariable('period_id','i',('ID','lat','lon',))
-        outVar.long_name='original period id'
-        outVar[:] = original_period_id[:,:,:]
+            outVar = nc_out.createVariable('hottest_day_temp','f',('ID','lat','lon',))
+            outVar.long_name='temperature of hottest day in period'
+            outVar[:] = hot_temp[:,:,:]
 
-        outVar = nc_out.createVariable('tasX','f',('ID','lat','lon',))
-        outVar.long_name='warmest day in the year'
-        outVar[:] = tasX[:,:,:]
+            outVar = nc_out.createVariable('period_id','i',('ID','lat','lon',))
+            outVar.long_name='original period id'
+            outVar[:] = original_period_id[:,:,:]
 
-        nc_out.close()
-        nc_in.close()
-        print time.time()-time0
+            outVar = nc_out.createVariable('tasX','f',('ID','lat','lon',))
+            outVar.long_name='warmest day in the year'
+            outVar[:] = tasX[:,:,:]
+
+            nc_out.close()
+            nc_in.close()
+            print time.time()-time0
 
 
 
