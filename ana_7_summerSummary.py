93780453625d9341cb737721f5a402f700fb0032
Peter <peterpfleiderer@gmx.de>
2017-11-28 10:31:03 +0100

    typo

--- ana_7_summerSummary.py
+++ ana_7_summerSummary.py
@@ -22,8 +22,8 @@ summary=da.DimArray(axes=[np.asarray(['Plus20-Future','Plus15-Future','All-Hist'
 for region in summary.region:
     dat=da.read_nc(working_path+region+'_'+model+'_summer.nc')
     summary[:,region,'mean_hot_shift']=dat['90X_hot_shift'].mean(axis='ID', skipna=True)
-    summary[:,region,'mean_hot_temp']=dat['90X_hot_temp'][scenario,:].mean(axis='ID', skipna=True)
-    summary[:,region,'mean_cum_heat']=dat['90X_cum_heat'][scenario,:].mean(axis='ID', skipna=True)
+    summary[:,region,'mean_hot_temp']=dat['90X_hot_temp'].mean(axis='ID', skipna=True)
+    summary[:,region,'mean_cum_heat']=dat['90X_cum_heat'].mean(axis='ID', skipna=True)
     for scenario in summary.scenario:
         summary[scenario,region,'frac_pos_shift']=len(np.where(dat['90X_hot_shift'][scenario,:]>0)[0])/float(dat['90X_hot_shift'].shape[1])
         print np.nanmean(dat['90X_hot_temp'][scenario,:])
