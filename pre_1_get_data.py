148f25d5642e86e32ef79fc8dc03ac9085434502
Peter <peterpfleiderer@gmx.de>
2017-11-29 11:14:08 +0100

    v1-0 for NorESM1

--- pre_1_get_data.py
+++ pre_1_get_data.py
@@ -7,7 +7,7 @@ import dimarray as da
 
 
 model_dict={'MIROC5':{'grid':'128x256','working_path':'/global/cscratch1/sd/pepflei/MIROC5/','in_path':'/project/projectdirs/m1517/C20C/MIROC/MIROC5/','version':{'Plus20-Future':'v2-0','Plus15-Future':'v2-0','All-Hist':'v1-0'}},
-			'NorESM1':{'grid':'192x288','working_path':'/global/cscratch1/sd/pepflei/NorESM1-HAPPI/','in_path':'/project/projectdirs/m1517/C20C/NCC/NorESM1-HAPPI/','version':{'Plus20-Future':'v2-0','Plus15-Future':'v2-0','All-Hist':'v1-0'}},
+			'NorESM1':{'grid':'192x288','working_path':'/global/cscratch1/sd/pepflei/NorESM1-HAPPI/','in_path':'/project/projectdirs/m1517/C20C/NCC/NorESM1-HAPPI/','version':{'Plus20-Future':'v1-0','Plus15-Future':'v1-0','All-Hist':'v1-0'}},
 			'ECHAM6-3-LR':{'grid':'96x192','working_path':'/global/cscratch1/sd/pepflei/ECHAM6-3-LR/','in_path':'/project/projectdirs/m1517/C20C/MPI-M/ECHAM6-3-LR/','version':{'Plus20-Future':'v2-0','Plus15-Future':'v2-0','All-Hist':'v1-0'}},
 			'CAM4-2degree':{'grid':'96x144','working_path':'/global/cscratch1/sd/pepflei/CAM4-2degree/','in_path':'/project/projectdirs/m1517/C20C/ETH/CAM4-2degree/','version':{'Plus20-Future':'v2-0','Plus15-Future':'v2-0','All-Hist':'v1-0'}},
 }
