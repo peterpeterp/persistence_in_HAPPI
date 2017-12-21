adcbf17466a57af832a346e5fd7adc972720f446
Peter <peterpfleiderer@gmx.de>
2017-12-21 13:26:17 +0100

    qunatiles assignment

--- ana_7_summerSummary.py
+++ ana_7_summerSummary.py
@@ -20,7 +20,8 @@ for region in summary.region:
     dat=da.read_nc(working_path+region+'_'+model+'_summer.nc')
     for var in ['90X_cum_heat','90X_hot_shift','90X_hot_temp','90X_mean_temp']:
         summary[:,region,var,'mean']=dat[var].mean(axis='ID', skipna=True)
-        summary[:,region,var,['qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu100']]=np.nanpercentile(dat[var].values,[0,1/6.*100,25,50,75,5/6.*100,100],axis=1)
+        for qu,qu_name in zip([0,1/6.*100,25,50,75,5/6.*100,100],['mean','qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu100']):
+            summary[:,region,var,qu_name]=np.nanpercentile(dat[var].values,qu,axis=1)
 
     for scenario in summary.scenario:
         summary[scenario,region,'frac_pos_shift','mean']=len(np.where(dat['90X_hot_shift'][scenario,:]>0)[0])/float(dat['90X_hot_shift'].shape[1])
