c58f5a638f5c8e85a0df86c3dc1d61c3273e8aab
Peter <peterpfleiderer@gmx.de>
2017-12-21 12:05:10 +0100

    assigning

--- ana_2_mean_qu.py
+++ ana_2_mean_qu.py
@@ -71,10 +71,10 @@ for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
 				if len(counter)>5:
 					cold,warm=counter_to_list(counter)
 					for state_name,distr in zip(['cold','warm'],[cold,warm]):
-						SummaryMeanQu[scenario][season][state_name]['mean'][lat[iy]][lon[ix]]=np.mean(distr)
+						SummaryMeanQu[scenario,season,state_name,'mean',lat[iy],lon[ix]]=np.mean(distr)
 						#SummaryMeanQu[scenario][season][state_name].ix[1:10,iy,ix]=np.percentile(distr,[1.,5.,10.,25.,50.,75.,90.,95.,99.])
-						SummaryMeanQu[scenario][season][state_name].ix[1:10,iy,ix]=quantile_from_cdf(distr,[1.,5.,10.,25.,50.,75.,90.,95.,99.])
-						SummaryMeanQu[scenario][season][state_name].ix[11:20,iy,ix]=np.nanpercentile(distr,[1.,5.,10.,25.,50.,75.,90.,95.,99.])
+						SummaryMeanQu[scenario,season,state_name,['qu_1','qu_5','qu_10','qu_25','qu_50','qu_75','qu_90','qu_95','qu_99'],lat[iy],lon[ix]]=quantile_from_cdf(distr,[1.,5.,10.,25.,50.,75.,90.,95.,99.])
+						SummaryMeanQu[scenario,season,state_name,['npqu_1','npqu_5','npqu_10','npqu_25','npqu_50','npqu_75','npqu_90','npqu_95','npqu_99'],lat[iy],lon[ix]]=np.nanpercentile(distr,[1.,5.,10.,25.,50.,75.,90.,95.,99.])
 
 ds=da.Dataset({'SummaryMeanQu':SummaryMeanQu})
 ds.write_nc('data/'+model+'_SummaryMeanQu.nc', mode='w')
