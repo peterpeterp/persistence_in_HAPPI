23697ea1beb143a6083b9bf1a63df25c9aa30b92
Peter <peterpfleiderer@gmx.de>
2017-12-22 11:28:43 +0100

    forgot to remove schenario

--- ana_7_summerSummary.py
+++ ana_7_summerSummary.py
@@ -19,8 +19,8 @@ summary=da.DimArray(axes=[['Plus20-Future','Plus15-Future','All-Hist'],list(srex
 for region in summary.region:
     dat=da.read_nc(working_path+region+'_'+model+'_summer.nc')
     for var in ['90X_mean_temp','90X_cum_heat','90X_hot_shift','90X_hot_temp']:
-        summary[scenario,region,var,'mean']=np.nanmean(dat[var],axis=1)
-        summary[scenario,region,var,['qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']]=np.nanpercentile(values,[0,1/6.*100,25,50,75,5/6.*100,100],axis=1)
+        summary[:,region,var,'mean']=np.nanmean(dat[var],axis=1)
+        summary[:,region,var,['qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']]=np.nanpercentile(values,[0,1/6.*100,25,50,75,5/6.*100,100],axis=1)
 
     for scenario in summary.scenario:
         summary[scenario,region,'frac_pos_shift','mean']=len(np.where(dat['90X_hot_shift'][scenario,:]>0)[0])/float(dat['90X_hot_shift'].shape[1])
