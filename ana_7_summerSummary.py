fdb05f97a47bc47e9ed14632bda3441bdf875158
Peter <peterpfleiderer@gmx.de>
2017-12-22 11:09:07 +0100

    better safe than elegant

--- ana_7_summerSummary.py
+++ ana_7_summerSummary.py
@@ -19,11 +19,12 @@ summary=da.DimArray(axes=[['Plus20-Future','Plus15-Future','All-Hist'],list(srex
 for region in summary.region:
     dat=da.read_nc(working_path+region+'_'+model+'_summer.nc')
     for var in ['90X_mean_temp','90X_cum_heat','90X_hot_shift','90X_hot_temp']:
-        values=dat[var].values
-        values=values[np.isfinte(values)]
-        summary[:,region,var,'mean']=np.nanmean(values,axis=1)
-        for qu,qu_name in zip([0,1/6.*100,25,50,75,5/6.*100,100],['mean','qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']):
-            summary[:,region,var,qu_name]=np.nanpercentile(values,qu,axis=1)
+        for scenario in summary.scenario:
+            values=dat[var][scenario,:].values
+            values=values[np.isfinte(values)]
+            summary[scenario,region,var,'mean']=np.nanmean(values,axis=1)
+            for qu,qu_name in zip([0,1/6.*100,25,50,75,5/6.*100,100],['mean','qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']):
+                summary[scenario,region,var,qu_name]=np.nanpercentile(values,qu,axis=1)
 
     for scenario in summary.scenario:
         summary[scenario,region,'frac_pos_shift','mean']=len(np.where(dat['90X_hot_shift'][scenario,:]>0)[0])/float(dat['90X_hot_shift'].shape[1])
