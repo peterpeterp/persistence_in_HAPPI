93eac96d5b494d1baf73a22e28c573217334d093
Peter <peterpfleiderer@gmx.de>
2018-01-02 15:46:02 +0100

    wrong length of initiated array

--- ana_6_summer_reg.py
+++ ana_6_summer_reg.py
@@ -43,6 +43,9 @@ def create_regional_distr(region):
 		for scenario in scenarios:
 			print working_path+'/'+model+'_'+scenario+'_summerQ90.nc'
 			data=da.read_nc(working_path+'/'+model+'_'+scenario+'_summerQ90.nc')
+			print data
+			print data['TXx_in90Xpers'].shape
+
 			tmp[scenario]={'90X_cum_heat':np.array([]),'90X_hot_shift':np.array([]),'90X_hot_temp':np.array([]),'90X_mean_temp':np.array([]),'TXx_in90Xpers':np.array([])}
 			polygon=Polygon(srex[region]['points'])
 			for x in data.lon:
@@ -67,9 +70,14 @@ def create_regional_distr(region):
 			for scenario in scenarios:
 				reg_dict[var][scenario,0:len(tmp[scenario][var])-1]=tmp[scenario][var]
 
-		reg_dict['TXx_in90Xpers']=da.DimArray(axes=[np.asarray(scenarios),np.array(range(1000))],dims=['scenario','ID'])
+		n_events=0
 		for scenario in scenarios:
-			reg_dict['TXx_in90Xpers'][scenario,:]=tmp[scenario]['TXx_in90Xpers']
+			if len(tmp[scenario]['TXx_in90Xpers'])>n_events:
+				n_events=len(tmp[scenario]['TXx_in90Xpers'])
+
+		reg_dict['TXx_in90Xpers']=da.DimArray(axes=[np.asarray(scenarios),np.array(range(n_events))],dims=['scenario','ID'])
+		for scenario in scenarios:
+			reg_dict['TXx_in90Xpers'][scenario,0:len(tmp[scenario][['TXx_in90Xpers'])-1]=tmp[scenario]['TXx_in90Xpers']
 
 		ds=da.Dataset(reg_dict)
 		ds.write_nc(working_path+'/regional/'+region+'_'+model+'_summer.nc', mode='w')
