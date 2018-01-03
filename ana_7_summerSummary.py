917d3c3173d3088705b240e9f3ddca084fbfde05
Peter <peterpfleiderer@gmx.de>
2018-01-03 15:11:24 +0100

    frac of TXx in 90XPers

--- ana_7_summerSummary.py
+++ ana_7_summerSummary.py
@@ -15,7 +15,7 @@ working_path='/global/cscratch1/sd/pepflei/'+model+'/regional/'
 pkl_file = open('data/srex_dict.pkl', 'rb')
 srex = pickle.load(pkl_file)	;	pkl_file.close()
 
-summary=da.DimArray(axes=[['Plus20-Future','Plus15-Future','All-Hist'],list(srex.keys()),['90X_cum_heat','90X_hot_shift','90X_hot_temp','90X_mean_temp','frac_pos_shift','frac_neg_shift'],['mean','qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']],dims=['scenario','region','variable','stat'])
+summary=da.DimArray(axes=[['Plus20-Future','Plus15-Future','All-Hist'],list(srex.keys()),['90X_cum_heat','90X_hot_shift','90X_hot_temp','90X_mean_temp','frac_pos_shift','frac_neg_shift','frac_TXx_inX90pers'],['mean','qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']],dims=['scenario','region','variable','stat'])
 for region in summary.region:
     dat=da.read_nc(working_path+region+'_'+model+'_summer.nc')
     for var in ['90X_mean_temp','90X_cum_heat','90X_hot_shift','90X_hot_temp']:
@@ -26,6 +26,9 @@ for region in summary.region:
     for scenario in summary.scenario:
         summary[scenario,region,'frac_pos_shift','mean']=len(np.where(dat['90X_hot_shift'][scenario,:]>0)[0])/float(dat['90X_hot_shift'].shape[1])
         summary[scenario,region,'frac_neg_shift','mean']=len(np.where(dat['90X_hot_shift'][scenario,:]<0)[0])/float(dat['90X_hot_shift'].shape[1])
+
+    for scenario in summary.scenario:
+        summary[scenario,region,'frac_TXx_inX90pers','mean']=len(np.where(dat['TXx_in90Xpers'][scenario,:]==0)[0])/float(dat['TXx_in90Xpers'].shape[1])
 
 ds=da.Dataset({'summerStats':summary})
 ds.write_nc('data/'+model+'_SummarySummer.nc', mode='w')
