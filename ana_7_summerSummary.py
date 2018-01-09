350f77460f2192bde739f6074a1ff848a463356f
Peter <peterpfleiderer@gmx.de>
2018-01-09 13:47:11 +0100

    correct variable names

--- ana_7_summerSummary.py
+++ ana_7_summerSummary.py
@@ -15,10 +15,10 @@ working_path='/global/cscratch1/sd/pepflei/'+model+'/regional/'
 pkl_file = open('data/SREX.pkl', 'rb')
 srex = pickle.load(pkl_file)	;	pkl_file.close()
 
-summary=da.DimArray(axes=[['Plus20-Future','Plus15-Future','All-Hist'],list(srex.keys()),['90X_cum_heat','90X_hot_shift','90X_hot_temp','90X_mean_temp','frac_pos_shift','frac_neg_shift','frac_TXx_inX90pers'],['mean','qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']],dims=['scenario','region','variable','stat'])
+summary=da.DimArray(axes=[['Plus20-Future','Plus15-Future','All-Hist'],list(srex.keys()),['x90_hottest_day','x90_cum_temp','x90_mean_temp','x90_hottest_day_shift','frac_pos_shift','frac_neg_shift','frac_TXx_in_X90'],['mean','qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']],dims=['scenario','region','variable','stat'])
 for region in summary.region:
     dat=da.read_nc(working_path+region+'_'+model+'_summer.nc')
-    for var in ['90X_mean_temp','90X_cum_heat','90X_hot_shift','90X_hot_temp']:
+    for var in ['x90_hottest_day','x90_cum_temp','x90_mean_temp','x90_hottest_day_shift']:
         summary[:,region,var,'mean']=np.nanmean(dat[var],axis=1)
         for qu,qu_name in zip([0,1/6.*100,25,50,75,5/6.*100,100],['qu_0','qu_66l','qu_25','qu_50','qu_75','qu_66h','qu_100']):
             summary[:,region,var,qu_name]=np.nanpercentile(dat[var],qu,axis=1)
@@ -29,10 +29,10 @@ for region in summary.region:
 
     for scenario in summary.scenario:
         print region
-        tmp=dat['TXx_in90Xpers'][scenario,:].values
+        tmp=dat['TXx_in_x90'][scenario,:].values
         tmp=tmp[np.isfinite(tmp)]
-        summary[scenario,region,'frac_TXx_inX90pers','mean']=len(np.where(tmp==1)[0])/float(tmp.shape[0])
+        summary[scenario,region,'frac_TXx_in_X90','mean']=len(np.where(tmp==1)[0])/float(tmp.shape[0])
         print len(np.where(tmp==1)[0]),len(np.where(tmp==0)[0]),float(tmp.shape[0]),len(np.where(tmp)[0])/float(tmp.shape[0])
 
 ds=da.Dataset({'summerStats':summary})
-ds.write_nc('data/'+model+'_SummarySummer.nc', mode='w')
+ds.write_nc('data/'+model+'/'+model+'_SummarySummer.nc', mode='w')
