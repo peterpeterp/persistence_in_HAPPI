fad95785050d36a1fc437c245f09b44b94d17758
Peter <peterpfleiderer@gmx.de>
2018-04-03 16:15:28 +0200

    working in the model folder

--- ana_8_summer_temp.py
+++ ana_8_summer_temp.py
@@ -24,8 +24,9 @@ for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
 		os.system('mkdir tmp/runs')
 		all_files=[raw for raw in glob.glob(working_path+scenario+'/*') if len(raw.split('/')[-1].split('_'))==7]
 		for region in ['ALA','CGI','NEU','NAS','WNA','CNA','ENA','CEU','CAS','TIB','EAS','CAM','MED','WAS']:
-			for in_file in all_files:
-				os.system('cdo timmean -fldsum -mul -selmon,6,7,8 '+in_file+' -select,name='+region+' /global/homes/p/pepflei/masks/srex_mask_'+model+'.nc tmp/runs/'+in_file.split('/')[-1].replace('.nc','_reg_av_'+region+'.nc'))
-			os.system('cdo ensmean tmp/runs/* tmp/tas_'+region+'.nc')
+			for in_file in all_files[0:5]:
+				print('cdo timmean -fldsum -mul -selmon,6,7,8 '+in_file+' -select,name='+region+' /global/homes/p/pepflei/masks/srex_mask_'+model+'.nc '+working_path+'tmp/runs/'+in_file.split('/')[-1].replace('.nc','_reg_av_'+region+'.nc'))
+				os.system('cdo timmean -fldsum -mul -selmon,6,7,8 '+in_file+' -select,name='+region+' /global/homes/p/pepflei/masks/srex_mask_'+model+'.nc '+working_path+'tmp/runs/'+in_file.split('/')[-1].replace('.nc','_reg_av_'+region+'.nc'))
+			os.system('cdo ensmean '+working_path+'tmp/runs/* '+working_path+'tmp/tas_'+region+'.nc')
 			asdas
-			os.system('rm tmp/runs/*')
+			os.system('rm '+working_path+'tmp/runs/*')
