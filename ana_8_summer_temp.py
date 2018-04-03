7d97e7a44f153e8760aaf29457bada608b0dc7f8
Peter <peterpfleiderer@gmx.de>
2018-04-03 17:39:04 +0200

    arg pars

--- ana_8_summer_temp.py
+++ ana_8_summer_temp.py
@@ -16,15 +16,40 @@ model_dict={'MIROC5':{'grid':'128x256'},
 os.chdir('/global/homes/p/pepflei/')
 
 
-for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
-	for model in model_dict.keys():
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--overwrite",'-o', help="overwrite output files",action="store_true")
+parser.add_argument('--scenario','-s',help='scneario in Plus20-Future Plus15-Future All-Hist',required=False)
+parser.add_argument('--model','-m',help='model',required=False)
+parser.add_argument('--region','-r',help='srex region',required=False)
+args = parser.parse_args()
+
+if args.model is None:
+	models=model_dict.keys()
+else:
+	models=[args.model]
+
+if args.scenario is None:
+	scenarios=['Plus20-Future','Plus15-Future','All-Hist']
+else:
+	scenarios=[args.scenario]
+
+if args.region is None:
+	regions=['ALA','CGI','NEU','NAS','WNA','CNA','ENA','CEU','CAS','TIB','EAS','CAM','MED','WAS']
+else:
+	regions=[args.region]
+
+
+for scenario in scenarios:
+	for model in models:
 		print(model)
 		os.chdir('/global/cscratch1/sd/pepflei/'+model+'/')
 		os.system('mkdir tmp')
 		os.system('mkdir tmp/runs')
 		os.system('mkdir tmp/masks')
 		all_files=[raw for raw in glob.glob(scenario+'/*') if len(raw.split('/')[-1].split('_'))==7]
-		for region in ['ALA','CGI','NEU','NAS','WNA','CNA','ENA','CEU','CAS','TIB','EAS','CAM','MED','WAS']:
+		for region in regions:
 			os.system('cdo select,name='+region+' /global/homes/p/pepflei/masks/srex_mask_'+model+'.nc tmp/masks/'+region+'.nc')
 			for id_,in_file in zip([str(ii) for ii in range(len(all_files[:]))],all_files[:]):
 				os.system('cdo selmon,6,7,8 '+in_file+' tmp/runs/tmp_'+id_+'_'+scenario+'.nc')
