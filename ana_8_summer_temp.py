57b2c064f6ac67ce5110df8b8b0e255a1c27bbc0
Peter <peterpfleiderer@gmx.de>
2018-04-03 16:24:10 +0200

    just move into working dir

--- ana_8_summer_temp.py
+++ ana_8_summer_temp.py
@@ -19,14 +19,14 @@ os.chdir('/global/homes/p/pepflei/')
 for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
 	for model in model_dict.keys():
 		print(model)
-		working_path='/global/cscratch1/sd/pepflei/'+model+'/'
+		os.chdir('/global/homes/p/pepflei/'+model+'/')
 		os.system('mkdir tmp')
 		os.system('mkdir tmp/runs')
 		all_files=[raw for raw in glob.glob(working_path+scenario+'/*') if len(raw.split('/')[-1].split('_'))==7]
 		for region in ['ALA','CGI','NEU','NAS','WNA','CNA','ENA','CEU','CAS','TIB','EAS','CAM','MED','WAS']:
 			for in_file in all_files[0:5]:
-				print('cdo timmean -fldsum -mul -selmon,6,7,8 '+in_file+' -select,name='+region+' /global/homes/p/pepflei/masks/srex_mask_'+model+'.nc '+working_path+'tmp/runs/'+in_file.split('/')[-1].replace('.nc','_reg_av_'+region+'.nc'))
-				os.system('cdo timmean -fldsum -mul -selmon,6,7,8 '+in_file+' -select,name='+region+' /global/homes/p/pepflei/masks/srex_mask_'+model+'.nc '+working_path+'tmp/runs/'+in_file.split('/')[-1].replace('.nc','_reg_av_'+region+'.nc'))
-			os.system('cdo ensmean '+working_path+'tmp/runs/* '+working_path+'tmp/tas_'+region+'.nc')
+				print('cdo timmean -fldsum -mul -selmon,6,7,8 '+in_file+' -select,name='+region+' /global/homes/p/pepflei/masks/srex_mask_'+model+'.nc tmp/runs/'+in_file.split('/')[-1].replace('.nc','_reg_av_'+region+'.nc'))
+				os.system('cdo timmean -fldsum -mul -selmon,6,7,8 '+in_file+' -select,name='+region+' /global/homes/p/pepflei/masks/srex_mask_'+model+'.nc tmp/runs/'+in_file.split('/')[-1].replace('.nc','_reg_av_'+region+'.nc'))
+			os.system('cdo ensmean tmp/runs/* tmp/tas_'+region+'.nc')
 			asdas
-			os.system('rm '+working_path+'tmp/runs/*')
+			os.system('rm tmp/runs/*')
