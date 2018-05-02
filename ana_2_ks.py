8a17f6be47a2fa52bef8a4f8d9094a717220bb5f
Peter <peterpfleiderer@gmx.de>
2018-05-02 13:18:59 +0200

    right paths

--- ana_2_ks.py
+++ ana_2_ks.py
@@ -35,7 +35,7 @@ types=['KS_vs_Plus20-Future','KS_vs_Plus15-Future','KS_vs_All-Hist']
 
 big_dict={}
 for scenario in ['All-Hist','Plus15-Future','Plus20-Future']:
-	pkl_file = open('data/'+model+'_'+scenario+'_counter.pkl', 'rb')
+	pkl_file = open('data/'+model+'/'+model+'_'+scenario+'_counter.pkl', 'rb')
 	big_dict[scenario] = pickle.load(pkl_file)	;	pkl_file.close()
 
 lat=big_dict[scenario]['lat']
@@ -81,4 +81,4 @@ for scenario_combi in [['Plus20-Future','All-Hist'],['Plus15-Future','All-Hist']
 								SummaryKS[scenario_store][season]['stateInd']['KS_vs_'+scenario_compare_to][lat[iy]][lon[ix]]=ks_stateInd
 
 ds=da.Dataset({'SummaryKS':SummaryKS})
-ds.write_nc('data/'+model+'_SummaryKS.nc', mode='w')
+ds.write_nc('data/'+model+'/'+model+'_SummaryKS.nc', mode='w')
