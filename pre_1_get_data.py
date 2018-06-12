2c031dc9269e3c39c83124db3bce5c6b31f0e5b8
Peter <peterpfleiderer@gmx.de>
2018-06-12 09:59:48 +0200

    do it only if spi and eke exist

--- pre_1_get_data.py
+++ pre_1_get_data.py
@@ -97,10 +97,9 @@ for scenario,selyears in zip(['Plus20-Future','Plus15-Future','All-Hist'],['2106
 		eke,spi=None,None
 		if os.path.isfile('/global/cscratch1/sd/pepflei/EKE/'+model+'/'+scenario+'/monEKE_'+model+'_'+scenario+'_'+run+'.nc'):
 			eke=da.read_nc('/global/cscratch1/sd/pepflei/EKE/'+model+'/'+scenario+'/monEKE_'+model+'_'+scenario+'_'+run+'.nc')['EKE'].values.squeeze()
-		if os.path.isfile('/global/cscratch1/sd/pepflei/SPI/'+model+'/'+scenario+'/SPI_'+model+'_'+scenario+'_'+run+'.nc'):
-			spi=da.read_nc('/global/cscratch1/sd/pepflei/SPI/'+model+'/'+scenario+'/SPI_'+model+'_'+scenario+'_'+run+'.nc')['SPI'].values.squeeze()
-		get_persistence(state_file,raw_file.replace('.nc','_period.nc'),overwrite=True,EKE=eke,SPI=spi)
-		print 'processing time:',time.time()-start_time
+			if os.path.isfile('/global/cscratch1/sd/pepflei/SPI/'+model+'/'+scenario+'/SPI_'+model+'_'+scenario+'_'+run+'.nc'):
+				spi=da.read_nc('/global/cscratch1/sd/pepflei/SPI/'+model+'/'+scenario+'/SPI_'+model+'_'+scenario+'_'+run+'.nc')['SPI'].values.squeeze()
+					get_persistence(state_file,raw_file.replace('.nc','_period.nc'),overwrite=True,EKE=eke,SPI=spi)
 
 		if os.path.isfile(raw_file.replace('.nc','_period.nc')):
 			print run,' processing time:',time.time()-start_time
