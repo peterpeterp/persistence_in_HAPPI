9f4034c0db237316a1bf5b532f41804a0ec3ee33
Peter <peterpfleiderer@gmx.de>
2018-06-19 15:50:42 +0200

    run_list

--- add_72_summaryCorrelation.py
+++ add_72_summaryCorrelation.py
@@ -28,8 +28,8 @@ except:
 
 for scenario in ['All-Hist','Plus20-Future']:
 	for cortype in ['corEKE','corSPI']:
-		run_list=sorted([path.split('/')[-1].split('_')[-1].split('.')[-1] for path in glob.glob(working_path+scenario+'/'+cortype+'*.nc')])
-
+		run_list=sorted([path.split('/')[-1].split('_')[-1].split('.')[0] for path in glob.glob(working_path+scenario+'/'+cortype+'*.nc')])
+		print(run_list)
 		example_data=da.read_nc(working_path+scenario+'/corEKE_'+'_'.join([model,scenario,run_list[0]])+'.nc')
 		summary=da.DimArray(axes=[run_list,range(4),[-1,1],example_data.lat,example_data.lon],dims=['run','season','state','lat','lon'])
 
