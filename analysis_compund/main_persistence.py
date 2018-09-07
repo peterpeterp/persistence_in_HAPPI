8dd29f4e6ac42ff1701dbf9a8fcecfe854b585c3
peterpeterp <peterpfleiderer@gmx.de>
2018-09-07 17:23:54 +0200

    more trials

--- analysis_compund/main_persistence.py
+++ analysis_compund/main_persistence.py
@@ -83,21 +83,21 @@ for scenario,selyears in zip(['Plus20-Future','Plus15-Future','All-Hist'],['2106
 				b=raw_file.replace('.nc','_b.nc')
 				result=try_several_times('cdo -O trend '+land_file+' '+a+' '+b)
 				detrend_1=raw_file.replace('.nc','_detrend_1.nc')
-				result=try_several_times('cdo -O subtrend '+land_file+' '+a+' '+b+' '+detrend_1,1,120)
+				result=try_several_times('cdo -O subtrend '+land_file+' '+a+' '+b+' '+detrend_1,2,120)
 
 				runmean_tmp=raw_file.replace('.nc','_runmean_tmp.nc')
-				result=try_several_times('cdo -O runmean,90 '+detrend_1+' '+runmean_tmp,1,120)
+				result=try_several_times('cdo -O runmean,90 '+detrend_1+' '+runmean_tmp,2,120)
 
 				empties=raw_file.replace('.nc','_empties.nc')
 				command='cdo -O -setrtomiss,-9999,9999 -seltimestep,'
 				for i in range(1,46,1): command+=str(i)+','
 				for i in range(3606,3652,1): command+=str(i)+','
-				result=try_several_times(command+' '+raw_file+' '+empties,1,120)
+				result=try_several_times(command+' '+raw_file+' '+empties,2,120)
 				runmean=raw_file.replace('.nc','_runmean.nc')
-				result=try_several_times('cdo -O mergetime '+empties+' '+runmean_tmp+' '+runmean,1,120)
+				result=try_several_times('cdo -O mergetime '+empties+' '+runmean_tmp+' '+runmean,2,120)
 
 				anom_file=raw_file.replace('.nc','_anom.nc')
-				result=try_several_times('cdo -O sub '+detrend_1+' '+runmean+' '+anom_file,1,120)
+				result=try_several_times('cdo -O sub '+detrend_1+' '+runmean+' '+anom_file,2,120)
 
 				# # state
 				temp_anomaly_to_ind(anom_file,tas_state_file,overwrite=True)
