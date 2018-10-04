6b9df42075cb9a495ac0a2f5cd0183f1a099c868
peterpeterp <peterpfleiderer@gmx.de>
2018-10-04 13:55:33 +0200

    debugging

--- analysis_compund/main_persistence.py
+++ analysis_compund/main_persistence.py
@@ -144,7 +144,9 @@ for scenario,selyears in zip(['Plus20-Future','Plus15-Future','All-Hist'],['2106
 					pr_hist_file = glob.glob(working_path+'All-Hist'+'/pr_*_'+run+'.nc')[0]
 					pr_percentile_file = pr_hist_file.replace('.nc','_percentile1mm.nc')
 					if os.path.isfile(pr_percentile_file) is False:
-						result=try_several_times('cdo chname,pr,qu -divc,36.5 -timsum -setrtoc,0,0.00001157407407407407,0 -setrtoc,0.00001157407407407407,9999,1 ' + pr_hist_file + ' ' + pr_percentile_file)
+						result=try_several_times('cdo chname,pr,qu -divc,36.5 -timsum -setrtoc,0,1,0 -setrtoc,1,9999,1 -mulc,86400 ' + pr_hist_file + ' ' + pr_percentile_file)
+						
+					result=try_several_times('cdo chname,pr,qu -divc,36.5 -timsum -setrtoc,0,1,0 -setrtoc,1,9999,1 -mulc,86400 ' + pr_hist_file + ' ' + pr_percentile_file)
 
 					precip_to_index_percentile(land_file,pr_state_file,pr_percentile_file,overwrite=True)
 
