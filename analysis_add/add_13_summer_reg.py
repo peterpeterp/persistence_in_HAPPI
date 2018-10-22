38dc5ae7030a5dcb45b92e0fa6fd68ad99244abd
peterpeterp <peterpfleiderer@gmx.de>
2018-10-22 11:32:28 +0200

    seasonal mean

--- analysis_add/add_13_summer_reg.py
+++ analysis_add/add_13_summer_reg.py
@@ -1,91 +1,42 @@
 import os,sys,glob,time,collections,gc
 import numpy as np
-from netCDF4 import Dataset,netcdftime,num2date
+from netCDF4 import Dataset,num2date
 import cPickle as pickle
 import dimarray as da
-from shapely.geometry import Point
-from shapely.geometry.polygon import Polygon
-
-sys.path.append('/global/homes/p/pepflei/weather_persistence/')
-sys.path.append('/Users/peterpfleiderer/Documents/Projects/Persistence/weather_persistence/')
-import persistence_support as persistence_support; reload(persistence_support)
-from persistence_support import *
-
-model=sys.argv[1]
-print model
-
-try:
-	region=sys.argv[2]
-except: region=None
 
 try:
 	os.chdir('/global/homes/p/pepflei/')
-	working_path='/global/cscratch1/sd/pepflei/'+model+'/'
-	scenarios=['Plus20-Future','Plus15-Future','All-Hist']
 except:
-	os.chdir('/Users/peterpfleiderer/Documents/Projects/Persistence/')
-	working_path='/Users/peterpfleiderer/Documents/Projects/Persistence/data/'+model+'/'
-	scenarios=['All-Hist']
+	os.chdir('/Users/peterpfleiderer/Projects/Persistence/')
 
-overwrite=False
-os.system('mkdir '+working_path+'/regional')
+sys.path.append('weather_persistence/')
+import persistence_support as persistence_support; reload(persistence_support)
+from persistence_support import *
 
-pkl_file = open('data/SREX.pkl', 'rb')
-srex = pickle.load(pkl_file)	;	pkl_file.close()
+sys.path.append('persistence_in_models/')
+import __settings
+model_dict=__settings.model_dict
 
+for model in ['CAM4-2degree','MIROC5','NorESM1','ECHAM6-3-LR']:
+	masks = da.read_nc('masks/srex_mask_'+model_dict[model]['grid']+'.nc')
+	for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
+		summerStat = da.read_nc('data/'+model+'/tas_'+model+'_'+scenario+'_summerStat.nc')
+		seasMean = da.read_nc('data/'+model+'/tas_'+model+'_'+scenario+'_seasMean.nc')
 
+		out_file={
+			'length' : da.DimArray(['7','14','21','28'],axes=[['7','14','21','28']],dims=['length']),
+		}
 
-print srex.keys()
+		for var in ['hottest_day','hottest_day_shift','mean_temp']:
+			out_file[var] = da.DimArray(axes=[masks.keys(),['7','14','21','28']],dims=['region','length'])
+			for region in masks.keys():
+				out_file[var][region,:] = np.nanmean(summerStat[var] * masks[region], axis=(1,2))
+			np.nanmean(summerStat[var][:,35:60,:], axis=(1,2))
 
-def create_regional_distr(region):
-	out_file=working_path+'/regional/'+region+'_'+model+'_summer.nc'
-	if overwrite and os.path.isfile(out_file): os.system('rm '+out_file)
-	if os.path.isfile(out_file)==False:
-		print region
-		tmp={}
-		for scenario in scenarios:
-			print working_path+'/'+model+'_'+scenario+'_summerQ90.nc'
-			data=da.read_nc(working_path+'/'+model+'_'+scenario+'_summerQ90.nc')
+			out_file[var] = da.DimArray(axes=[masks.keys(),['7','14','21','28']],dims=['region','length'])
+			for region in masks.keys():
+				out_file[var][region,:] = np.nanmean(summerStat[var] * masks[region], axis=(1,2))
+			np.nanmean(summerStat[var][:,35:60,:], axis=(1,2))
 
-			tmp[scenario]={'x90_cum_temp':np.array([]),'x90_mean_temp':np.array([]),'x90_hottest_day_shift':np.array([]),'x90_hottest_day':np.array([]),'TXx_in_x90':np.array([])}
-			polygon=Polygon(srex[region]['points'])
-			for x in data.lon:
-				if x>180:
-					x__=x-360
-				else:
-					x__=x
-				for y in data.lat:
-					if polygon.contains(Point(x__,y)):
-						for var in tmp[scenario].keys():
-							tmp[scenario][var]=np.append(tmp[scenario][var],data[var][:,:,y,x].flatten())
-
-		n_events=0
-		for var in ['x90_hottest_day','x90_cum_temp','x90_mean_temp','x90_hottest_day_shift']:
-			for scenario in scenarios:
-				if len(tmp[scenario][var])>n_events:
-					n_events=len(tmp[scenario][var])
-
-		reg_dict={}
-		for var in ['x90_hottest_day','x90_cum_temp','x90_mean_temp','x90_hottest_day_shift']:
-			reg_dict[var]=da.DimArray(axes=[np.asarray(scenarios),np.array(range(n_events))],dims=['scenario','ID'])
-			for scenario in scenarios:
-				reg_dict[var][scenario,0:len(tmp[scenario][var])-1]=tmp[scenario][var]
-
-		n_events=0
-		for scenario in scenarios:
-			if len(tmp[scenario]['TXx_in_x90'])>n_events:
-				n_events=len(tmp[scenario]['TXx_in_x90'])
-
-		reg_dict['TXx_in_x90']=da.DimArray(axes=[np.asarray(scenarios),np.array(range(n_events))],dims=['scenario','ID'])
-		for scenario in scenarios:
-			reg_dict['TXx_in_x90'][scenario,0:len(tmp[scenario]['TXx_in_x90'])-1]=tmp[scenario]['TXx_in_x90']
-
-		ds=da.Dataset(reg_dict)
-		ds.write_nc(working_path+'/regional/'+region+'_'+model+'_summer.nc', mode='w')
-
-
-if region is None:
-	for region in srex.keys():
-		create_regional_distr(region)
-else:
-	create_regional_distr(region)
+		out_file = da.Dataset(out_file)
+		out_file.write_nc('data/'+model+'/tas_'+model+'_'+scenario+'_summerStat_srex.nc','w')
---
+++ analysis_add/add_13_summer_reg__.py
@@ -0,0 +1,91 @@
+import os,sys,glob,time,collections,gc
+import numpy as np
+from netCDF4 import Dataset,netcdftime,num2date
+import cPickle as pickle
+import dimarray as da
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+sys.path.append('/global/homes/p/pepflei/weather_persistence/')
+sys.path.append('/Users/peterpfleiderer/Documents/Projects/Persistence/weather_persistence/')
+import persistence_support as persistence_support; reload(persistence_support)
+from persistence_support import *
+
+model=sys.argv[1]
+print model
+
+try:
+	region=sys.argv[2]
+except: region=None
+
+try:
+	os.chdir('/global/homes/p/pepflei/')
+	working_path='/global/cscratch1/sd/pepflei/'+model+'/'
+	scenarios=['Plus20-Future','Plus15-Future','All-Hist']
+except:
+	os.chdir('/Users/peterpfleiderer/Documents/Projects/Persistence/')
+	working_path='/Users/peterpfleiderer/Documents/Projects/Persistence/data/'+model+'/'
+	scenarios=['All-Hist']
+
+overwrite=False
+os.system('mkdir '+working_path+'/regional')
+
+pkl_file = open('data/SREX.pkl', 'rb')
+srex = pickle.load(pkl_file)	;	pkl_file.close()
+
+
+
+print srex.keys()
+
+def create_regional_distr(region):
+	out_file=working_path+'/regional/'+region+'_'+model+'_summer.nc'
+	if overwrite and os.path.isfile(out_file): os.system('rm '+out_file)
+	if os.path.isfile(out_file)==False:
+		print region
+		tmp={}
+		for scenario in scenarios:
+			print working_path+'/'+model+'_'+scenario+'_summerQ90.nc'
+			data=da.read_nc(working_path+'/'+model+'_'+scenario+'_summerQ90.nc')
+
+			tmp[scenario]={'x90_cum_temp':np.array([]),'x90_mean_temp':np.array([]),'x90_hottest_day_shift':np.array([]),'x90_hottest_day':np.array([]),'TXx_in_x90':np.array([])}
+			polygon=Polygon(srex[region]['points'])
+			for x in data.lon:
+				if x>180:
+					x__=x-360
+				else:
+					x__=x
+				for y in data.lat:
+					if polygon.contains(Point(x__,y)):
+						for var in tmp[scenario].keys():
+							tmp[scenario][var]=np.append(tmp[scenario][var],data[var][:,:,y,x].flatten())
+
+		n_events=0
+		for var in ['x90_hottest_day','x90_cum_temp','x90_mean_temp','x90_hottest_day_shift']:
+			for scenario in scenarios:
+				if len(tmp[scenario][var])>n_events:
+					n_events=len(tmp[scenario][var])
+
+		reg_dict={}
+		for var in ['x90_hottest_day','x90_cum_temp','x90_mean_temp','x90_hottest_day_shift']:
+			reg_dict[var]=da.DimArray(axes=[np.asarray(scenarios),np.array(range(n_events))],dims=['scenario','ID'])
+			for scenario in scenarios:
+				reg_dict[var][scenario,0:len(tmp[scenario][var])-1]=tmp[scenario][var]
+
+		n_events=0
+		for scenario in scenarios:
+			if len(tmp[scenario]['TXx_in_x90'])>n_events:
+				n_events=len(tmp[scenario]['TXx_in_x90'])
+
+		reg_dict['TXx_in_x90']=da.DimArray(axes=[np.asarray(scenarios),np.array(range(n_events))],dims=['scenario','ID'])
+		for scenario in scenarios:
+			reg_dict['TXx_in_x90'][scenario,0:len(tmp[scenario]['TXx_in_x90'])-1]=tmp[scenario]['TXx_in_x90']
+
+		ds=da.Dataset(reg_dict)
+		ds.write_nc(working_path+'/regional/'+region+'_'+model+'_summer.nc', mode='w')
+
+
+if region is None:
+	for region in srex.keys():
+		create_regional_distr(region)
+else:
+	create_regional_distr(region)
