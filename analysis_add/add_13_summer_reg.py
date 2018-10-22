wrong masking

analysis_add/add_13_summer_reg.py
@@ -36,8 +36,7 @@ out_file['seasMean'] = da.DimArray(axes=[models,scenarios,regions],dims=['model'
 for model in models:
 	masks = da.read_nc('masks/srex_mask_'+model_dict[model]['grid']+'.nc')
 	for name,mask in masks.items():
-		mask[mask>0] = 1
-		masks[name] = mask
+		mask[mask==0] = np.nan
 	for scenario in scenarios:
 		summerStat = da.read_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_summerStat.nc')
 		seasMean = da.read_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_seasMean.nc')['tas'].squeeze().ix[2]