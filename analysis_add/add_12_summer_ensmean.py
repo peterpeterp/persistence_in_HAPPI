31ebcf2c6a395502d3c60b97a51b1ea685de8ad1
peterpeterp <peterpfleiderer@gmx.de>
2018-10-22 12:00:01 +0200

    super condenced summer summary

--- analysis_add/add_12_summer_ensmean.py
+++ analysis_add/add_12_summer_ensmean.py
@@ -61,31 +61,31 @@ grid=model_dict[model]['grid']
 for scenario,selyears in zip(['Plus20-Future','Plus15-Future','All-Hist'],['2106/2115','2106/2115','2006/2015']):
 	result=try_several_times('cdo ensmean /global/cscratch1/sd/pepflei/'+model+'/'+scenario+'/summerStat/*seasMean.nc data/'+model+'/summer/tas_'+model+'_'+scenario+'_seasMean.nc')
 
-# 	all_files=glob.glob(working_path+scenario+'/summerStat/tas_Aday_*_summer.nc')
-#
-# 	example_file=da.read_nc(all_files[0])
-#
-# 	merged_=da.Dataset({
-# 		'hottest_day':da.DimArray(axes=[range(100),['7','14','21','28'],example_file.lat,example_file.lon],dims=['run','length','lat','lon']),
-# 		'mean_temp':da.DimArray(axes=[range(100),['7','14','21','28'],example_file.lat,example_file.lon],dims=['run','length','lat','lon']),
-# 		'hottest_day_shift':da.DimArray(axes=[range(100),['7','14','21','28'],example_file.lat,example_file.lon],dims=['run','length','lat','lon']),
-# 	})
-#
-# 	for id,in_file in enumerate(all_files):
-# 		print(id)
-# 		tmp = da.read_nc(in_file)
-#
-# 		for var in ['hottest_day','hottest_day_shift','mean_temp']:
-# 			merged_[var][id,:,:,:] = tmp[var]
-#
-# 	out_file=da.Dataset({
-# 		'length' : da.DimArray(['7','14','21','28'],axes=[['7','14','21','28']],dims=['length']),
-# 		'lon': example_file['lon'],
-# 		'lat': example_file['lat'],
-# 	})
-#
-# 	for var in ['hottest_day','hottest_day_shift','mean_temp']:
-# 		out_file[var] = da.DimArray(np.nanmean(merged_[var],axis=0),axes=[['7','14','21','28'],example_file.lat,example_file.lon],dims=['length','lat','lon'])
-#
-# 	out_file.write_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_summerStat.nc','w')
+	all_files=glob.glob(working_path+scenario+'/summerStat/tas_Aday_*_summer.nc')
+
+	example_file=da.read_nc(all_files[0])
+
+	merged_=da.Dataset({
+		'hottest_day':da.DimArray(axes=[range(100),['7','14','21','28'],example_file.lat,example_file.lon],dims=['run','length','lat','lon']),
+		'mean_temp':da.DimArray(axes=[range(100),['7','14','21','28'],example_file.lat,example_file.lon],dims=['run','length','lat','lon']),
+		'hottest_day_shift':da.DimArray(axes=[range(100),['7','14','21','28'],example_file.lat,example_file.lon],dims=['run','length','lat','lon']),
+	})
+
+	for id,in_file in enumerate(all_files):
+		print(id)
+		tmp = da.read_nc(in_file)
+
+		for var in ['hottest_day','hottest_day_shift','mean_temp']:
+			merged_[var][id,:,:,:] = tmp[var]
+
+	out_file=da.Dataset({
+		'length' : da.DimArray(['7','14','21','28'],axes=[['7','14','21','28']],dims=['length']),
+		'lon': example_file['lon'],
+		'lat': example_file['lat'],
+	})
+
+	for var in ['hottest_day','hottest_day_shift','mean_temp']:
+		out_file[var] = da.DimArray(np.nanmean(merged_[var],axis=0),axes=[['7','14','21','28'],example_file.lat,example_file.lon],dims=['length','lat','lon'])
+
+	out_file.write_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_summerStat.nc','w')
 # # #
--- analysis_add/add_13_summer_reg.py
+++ analysis_add/add_13_summer_reg.py
@@ -17,27 +17,37 @@ sys.path.append('persistence_in_models/')
 import __settings
 model_dict=__settings.model_dict
 
-for model in ['CAM4-2degree','MIROC5','NorESM1','ECHAM6-3-LR']:
+models = ['CAM4-2degree','MIROC5','NorESM1','ECHAM6-3-LR']
+scenarios = ['All-Hist','Plus20-Future','Plus15-Future']
+
+out_file={
+	'length' : da.DimArray(['7','14','21','28'],axes=[['7','14','21','28']],dims=['length']),
+	'model' : da.DimArray(models,axes=[models],dims=['model']),
+	'scenario' : da.DimArray(scenarios,axes=[scenarios],dims=['scenario']),
+}
+
+masks = da.read_nc('masks/srex_mask_'+model_dict['MIROC5']['grid']+'.nc')
+regions = masks.keys()+['NHml']
+for var in ['hottest_day','hottest_day_shift','mean_temp']:
+	out_file[var] = da.DimArray(axes=[models,scenarios,regions,['7','14','21','28']],dims=['model','scenario','region','length'])
+
+out_file['seasMean'] = da.DimArray(axes=[models,scenarios,regions],dims=['model','scenario','region'])
+
+for model in models:
 	masks = da.read_nc('masks/srex_mask_'+model_dict[model]['grid']+'.nc')
-	regions = masks.keys()+['NHml']
-	for scenario in ['All-Hist','Plus20-Future','Plus15-Future']:
+	for scenario in scenarios:
 		summerStat = da.read_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_summerStat.nc')
 		seasMean = da.read_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_seasMean.nc')['tas'].squeeze().ix[2]
 
-		out_file={
-			'length' : da.DimArray(['7','14','21','28'],axes=[['7','14','21','28']],dims=['length']),
-		}
-
 		for var in ['hottest_day','hottest_day_shift','mean_temp']:
-			out_file[var] = da.DimArray(axes=[regions,['7','14','21','28']],dims=['region','length'])
 			for region in masks.keys():
-				out_file[var][region,:].values = np.nanmean(summerStat[var] * masks[region], axis=(1,2))
-			out_file[var]['NHml',:].values = np.nanmean(summerStat[var][:,35:60,:], axis=(1,2))
+				out_file[var][model,scenario,region,:].values = np.nanmean(summerStat[var] * masks[region], axis=(1,2))
+			out_file[var][model,scenario,'NHml',:].values = np.nanmean(summerStat[var][:,35:60,:], axis=(1,2))
 
-		out_file['seasMean'] = da.DimArray(axes=[regions],dims=['region'])
+
 		for region in masks.keys():
-			out_file['seasMean'][region] = np.nanmean(seasMean * masks[region])
-		out_file['seasMean']['NHml'] = np.nanmean(seasMean[35:60,:])
+			out_file['seasMean'][model,scenario,region] = np.nanmean(seasMean * masks[region])
+		out_file['seasMean'][model,scenario,'NHml'] = np.nanmean(seasMean[35:60,:])
 
-		out_file = da.Dataset(out_file)
-		out_file.write_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_summerStat_srex.nc','w')
+out_file = da.Dataset(out_file)
+out_file.write_nc('data/tas_summerStat_srex.nc','w')
