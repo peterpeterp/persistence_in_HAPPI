bd5e8b977313dfb443270ac6b4c57183a709ceed
peterpeterp <peterpfleiderer@gmx.de>
2018-10-22 12:08:30 +0200

    masking style

--- analysis_add/add_13_summer_reg.py
+++ analysis_add/add_13_summer_reg.py
@@ -35,18 +35,21 @@ out_file['seasMean'] = da.DimArray(axes=[models,scenarios,regions],dims=['model'
 
 for model in models:
 	masks = da.read_nc('masks/srex_mask_'+model_dict[model]['grid']+'.nc')
+	for name,mask in masks.items():
+		mask[mask>0] = 1
 	for scenario in scenarios:
 		summerStat = da.read_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_summerStat.nc')
 		seasMean = da.read_nc('data/'+model+'/summer/tas_'+model+'_'+scenario+'_seasMean.nc')['tas'].squeeze().ix[2]
 
 		for var in ['hottest_day','hottest_day_shift','mean_temp']:
 			for region in masks.keys():
-				out_file[var][model,scenario,region,:].values = np.nanmean(summerStat[var].ix[np.where(masks[region]!=0)], axis=(1,2))
+				out_file[var][model,scenario,region,:].values = np.nanmean(summerStat[var] * masks[region], axis=(1,2))
 			out_file[var][model,scenario,'NHml',:].values = np.nanmean(summerStat[var][:,35:60,:], axis=(1,2))
 
 
 		for region in masks.keys():
-			out_file['seasMean'][model,scenario,region] = np.nanmean(seasMean.ix[np.where(masks[region]!=0)])
+			out_file['seasMean'][model,scenario,region] = np.nanmean(seasMean * masks[region])
+			print(out_file['seasMean'][model,scenario,region])
 		out_file['seasMean'][model,scenario,'NHml'] = np.nanmean(seasMean[35:60,:])
 
 out_file = da.Dataset(out_file)
