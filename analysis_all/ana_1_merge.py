150b9157d460ed4356969cd03e2df1f211072817
peterpeterp <peterpfleiderer@gmx.de>
2018-11-02 10:28:36 +0100

    checking hist

--- analysis_all/ana_1_merge.py
+++ analysis_all/ana_1_merge.py
@@ -14,7 +14,7 @@ working_path='/global/cscratch1/sd/pepflei/'+model+'/'
 seasons={'MAM':{'months':[3,4,5],'index':0}, 'JJA':{'months':[6,7,8],'index':1}, 'SON':{'months':[9,10,11],'index':2}, 'DJF':{'months':[12,1,2],'index':3}}
 
 for style,state in zip(['tas'],['warm']):
-	for scenario in ['Plus20-Future']:
+	for scenario in ['All-Hist']:
 		all_files=sorted(glob.glob(working_path+scenario+'/'+style+'/'+style+'*'+state+'.nc'))
 		print all_files
 
