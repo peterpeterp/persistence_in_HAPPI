17efbd16be6745e53b69cbc24e41e11350d3bbcd
peterpeterp <peterpfleiderer@gmx.de>
2018-11-02 17:44:30 +0100

    tas period

--- analysis_all/ana_3_reg.py
+++ analysis_all/ana_3_reg.py
@@ -17,18 +17,20 @@ except:
 	os.chdir('/global/homes/p/pepflei/')
 
 
-def get_regional_distribution(regions,model,style,states,scenarios=['All-Hist','Plus20-Future','Plus15-Future'],add_name=''):
+def get_regional_distribution(regions,model,style,add_name,states_to_analyze,scenarios=['All-Hist','Plus20-Future','Plus15-Future'],regions_id=''):
 	region_dict={}
 	for region in regions.keys():
 		region_dict[region]={}
 		for scenario in scenarios:
-			pkl_file = open('data/'+model+'/'+style+'_'+model+'_'+scenario+'_counter.pkl', 'rb')
+			pkl_file = open('data/'+model+'/'+style+'_'+model+'_'+scenario+'_'+add_name+'_counter.pkl', 'rb')
 			distr_dict = pickle.load(pkl_file)	;	pkl_file.close()
 			region_dict[region][scenario]={}
 			tmp={}
 			for season in ['MAM','JJA','SON','DJF']:
 				print region,scenario,season
-				region_dict[region][scenario][season]={states[0]:{},states[1]:{}}
+				region_dict[region][scenario][season]={}
+				for state_name in states_to_analyze.values():
+					region_dict[region][scenario][season][state_name]={}
 				tmp[season]=collections.Counter()
 			polygon=Polygon(regions[region]['points'])
 			for x in distr_dict['lon']:
@@ -39,18 +41,18 @@ def get_regional_distribution(regions,model,style,states,scenarios=['All-Hist','
 				for y in distr_dict['lat']:
 					if polygon.contains(Point(x__,y)):
 						for season in ['MAM','JJA','SON','DJF']:
-							if len(distr_dict[str(y)+'_'+str(x)][season].keys())>10:
+							if len(distr_dict[str(y)+'_'+str(x)][season].keys())>=2:
 								tmp[season]+=distr_dict[str(y)+'_'+str(x)][season]
 
 			for season in ['MAM','JJA','SON','DJF']:
-				if len(tmp[season])>5:
-					for state,state_name in zip([-1,1],states):
+				if len(tmp[season])>=2:
+					for state,state_name in states_to_analyze.items():
 						count,pers=counter_to_pers(tmp[season],state)
 						region_dict[region][scenario][season][state_name]['period_length']=pers
 						region_dict[region][scenario][season][state_name]['count']=count
 						region_dict[region][scenario][season]['counter']=tmp[season]
 
-	output = open('data/'+model+'/'+style+'_'+model+'_regional_distrs_'+add_name+'.pkl', 'wb')
+	output = open('data/'+model+'/'+style+'_'+add_name+'_'+model+'_regional_distrs_'+regions_id+'.pkl', 'wb')
 	pickle.dump(region_dict, output)
 	output.close()
 	return region_dict
@@ -61,11 +63,9 @@ print model
 
 # region_dict=get_regional_distribution({'mid-lat':{'points':[(-180,-23),(180,-23),(180,-66),(-180,-66)]}},model,add_name='mid-lat-SH')
 
-for style,states in zip(['tas','cpd','pr'],[['cold','warm'],['wet-cold','dry-warm'],['dry','wet']]):
-	region_dict=get_regional_distribution({'mid-lat':{'points':[(-180,35),(180,35),(180,60),(-180,60)]}},model,style,states,scenarios=['All-Hist','Plus20-Future'],add_name='mid-lat')
 
 pkl_file = open('data/srex_dict.pkl', 'rb')
 srex = pickle.load(pkl_file)	;	pkl_file.close()
+srex['mid-lat'] = {'points':[(-180,35),(180,35),(180,60),(-180,60)]}
 #srex = {key:value for key,value in srex.items() if key in ['CEU','NEU','NAS','MED','WAS']}
-for style,states in zip(['tas','cpd','pr'],[['cold','warm'],['wet-cold','dry-warm'],['dry','wet']]):
-	region_dict=get_regional_distribution(srex,model,style,states,scenarios=['All-Hist','Plus20-Future'],add_name='srex')
+region_dict=get_regional_distribution(regions=srex,model=model,style='pr',states_to_analyze={1:'5mm'},add_name='period5mm',scenarios=['All-Hist','Plus20-Future'],regions_id='srex')
