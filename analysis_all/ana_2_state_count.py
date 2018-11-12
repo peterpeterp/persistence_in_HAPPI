2b156959ef9a554ab6d41cf1c69753c91bec323b
peterpeterp <peterpfleiderer@gmx.de>
2018-11-12 13:56:53 +0100

    new state count

--- analysis_all/ana_2_state_count.py
+++ analysis_all/ana_2_state_count.py
@@ -1,73 +1,77 @@
-import os,sys,glob,time,collections,gc
+import os,sys,glob,time,collections,signal,gc
 import numpy as np
-from netCDF4 import Dataset,netcdftime,num2date
-import cPickle as pickle
+from netCDF4 import Dataset,num2date
+import random as random
 import dimarray as da
+import subprocess as sub
 
-try:
-	os.chdir('/Users/peterpfleiderer/Documents/Projects/HAPPI_persistence/')
-except:
-	os.chdir('/global/homes/p/pepflei/')
+def wait_timeout(proc, seconds):
+	"""Wait for a process to finish, or raise exception after timeout"""
+	start = time.time()
+	end = start + seconds
+	interval = min(seconds / 1000.0, .25)
 
-
-def counter_to_count(counter):
-	tmp=0
-	lengths=counter.keys()
-	if 0 in lengths:
-		lengths.remove(0)
-	if len(lengths)>2:
-		for key in lengths:
-			for i in range(counter[key]):
-				tmp+=key
-		tmp=np.array(tmp)
-		return tmp
-	else:
-		return 0
+	while True:
+		result = proc.poll()
+		if result is not None:
+			return result
+		if time.time() >= end:
+			os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+			return 'failed'
+		time.sleep(interval)
 
 
+def try_several_times(command,trials=2,seconds=60):
+	for trial in range(trials):
+		proc=sub.Popen(command,stdout=sub.PIPE,shell=True, preexec_fn=os.setsid)
+		result=wait_timeout(proc,seconds)
+		if result!='failed':
+			break
+	return(result)
+
+os.chdir('/global/homes/p/pepflei/')
 model=sys.argv[1]
-print model
+working_path='/global/cscratch1/sd/pepflei/'+model+'/'
+sys.path.append('/global/homes/p/pepflei/weather_persistence/')
+import persistence_functions as prsfc; reload(prsfc)
 
-scenarios=['All-Hist','Plus15-Future','Plus20-Future']
-seasons=['MAM','JJA','SON','DJF','year']
+sys.path.append('persistence_in_models/')
+import __settings
+model_dict=__settings.model_dict
 
-state_dict = {
-	'warm':'tas',
-	'dry':'pr',
-	'5mm':'pr',
-	'10mm':'pr',
-	'dry-warm':'cpd',
-	}
+style_dict = {
+	'pr':{'states':['dry','5mm','10mm'],'days':3650},
+	'tas':{'states':['warm'],'days':3650},
+	'cpd':{'states':['dry-warm'],'days':3650},
+}
 
-for state,style in state_dict.items():
+seasons={'MAM':[3,4,5],'JJA':[6,7,8],'SON':[9,10,11],'DJF':[12,1,2]}
 
-	big_dict={}
-	for scenario in scenarios:
-		pkl_file = open('data/'+model+'/'+style+'_'+model+'_'+scenario+'_'+state+'_counter.pkl', 'rb')
-		big_dict[scenario] = pickle.load(pkl_file)	;	pkl_file.close()
+for style,info in style_dict.items():	#,'cpd','tas'
+	for scenario in ['Plus20-Future','Plus15-Future','All-Hist']:
+		state_files = sorted(glob.glob(working_path+scenario+'/'+style+'/'+style+'_*_state.nc'))
+		for state_file in state_files:
+			print(state_file)
 
-	lat=big_dict[scenario]['lat']
-	lon=big_dict[scenario]['lon']
+			states = info['states']
+			nc = da.read_nc(state_file)
 
-	if 'SummaryMeanQu' not in globals():
-		SummaryMeanQu=da.DimArray(axes=[np.asarray(scenarios),np.asarray(seasons),np.asarray(state_dict.keys()),lat,lon], dims=['scenario','season','state','lat','lon'])
+			if 'calendar' in nc['time'].attrs.keys():
+				datevar=num2date(nc['time'].values,units = nc['time'].units, calendar = nc['time'].calendar)
+			else:
+				datevar=num2date(nc['time'].values,units = nc['time'].units)
+			month=np.array([date.month for date in datevar])
 
-	for scenario in scenarios:
-		distr_dict = big_dict[scenario]
+			if 'stateCount' not in globals():
+				stateCount = da.Dataset({})
+				for state in states:
+					stateCount[state] = da.DimArray(np.zeros([4,len(nc.lat),len(nc.lon)]), axes=[seasons.keys(),nc.lat,nc.lon],dims=['season','lat','lon'])
 
-		for iy in range(len(lat)):
-			for ix in range(len(lon)):
-				grid_cell=distr_dict[str(lat[iy])+'_'+str(lon[ix])]
-				grid_cell['year']=grid_cell['MAM']+grid_cell['JJA']+grid_cell['SON']+grid_cell['DJF']
+			# number of states
+			for state in states:
+				for season in seasons.keys():
+					days_in_season=np.where( (month==seasons[season][0]) | (month==seasons[season][1]) | (month==seasons[season][2]) )[0]
+					stateCount[state][season].values += np.nansum(nc[state].ix[days_in_season,:,:],axis=0)
 
-		for season in seasons:
-			print season
-			for iy in range(len(lat)):
-				sys.stdout.write('.')	;	sys.stdout.flush()
-				for ix in range(len(lon)):
-					counter=distr_dict[str(lat[iy])+'_'+str(lon[ix])][season]
-					if len(counter)>3:
-						SummaryMeanQu[scenario,season,state,lat[iy],lon[ix]]=counter_to_count(counter)
-
-ds=da.Dataset({'SummaryMeanQu':SummaryMeanQu})
-ds.write_nc('data/'+model+'/'+model+'_StateCount.nc', mode='w')
+		stateCount.write_nc('data/' + model + '/state_stats/' + style + '_' + model +'_' +scenario + '_stateCount.nc')
+		del stateCount
