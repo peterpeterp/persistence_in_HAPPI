9f9e2c6daab4fa25f245417b292e3b2f6b4f401f
peterpeterp <peterpfleiderer@gmx.de>
2019-03-06 11:06:02 +0100

    bit more time

--- analysis_add/add_31_EKE.py
+++ analysis_add/add_31_EKE.py
@@ -118,8 +118,8 @@ for scenario in scenarios:
 
 				if len(glob.glob(var+'*'+run+'*'))==1:
 					orig_file='_'.join(glob.glob(var+'*'+run+'*')[0].split('_')[:-1])+'.nc'
-					result=try_several_times('cdo -O -selyear,'+selyears+' '+glob.glob(var+'*'+run+'*')[0]+' '+orig_file.replace('.nc','_selyear.nc'),5,60)
-					result=try_several_times('cdo -O -sellevel,85000 '+orig_file.replace('.nc','_selyear.nc')+' '+orig_file,5,60)
+					result=try_several_times('cdo -O -selyear,'+selyears+' '+glob.glob(var+'*'+run+'*')[0]+' '+orig_file.replace('.nc','_selyear.nc'),2,120)
+					result=try_several_times('cdo -O -sellevel,85000 '+orig_file.replace('.nc','_selyear.nc')+' '+orig_file,2,120)
 					out=os.system('rm '+' '.join([orig_file.replace('.nc','_merged.nc'),orig_file.replace('.nc','_selyear.nc')]))
 
 				elif len(glob.glob(var+'*'+run+'*'))>1:
