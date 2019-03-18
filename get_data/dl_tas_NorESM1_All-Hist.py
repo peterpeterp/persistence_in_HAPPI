fa2e2e17586dd5ee46f5ae2b44ac68938027db3e
peterpeterp <peterpfleiderer@gmx.de>
2019-03-18 14:35:13 +0100

    downloading the correct run

--- get_data/dl_tas_NorESM1_All-Hist.py
+++ get_data/dl_tas_NorESM1_All-Hist.py
@@ -23,7 +23,7 @@ os.system('mkdir -p raw_data/'+model+'/'+scenario+'/'+var)
 os.chdir('raw_data/'+model+'/'+scenario+'/'+var)
 
 for run in model_dict[model]['runs'][scenario]:
-    filename = "tas_Aday_NorESM1-HAPPI_All-Hist_est1_v1-0_run121_20060101-20160630.nc"
+    filename = "tas_Aday_NorESM1-HAPPI_All-Hist_est1_v1-0_"+run+"_20060101-20160630.nc"
     print("downloading: "+filename)
     urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=NCC/NorESM1-HAPPI/All-Hist/est1/v1-0/day/atmos/tas/"+run+"/tas_Aday_NorESM1-HAPPI_All-Hist_est1_v1-0_"+run+"_20060101-20160630.nc",filename, hook)
     print('\n')
