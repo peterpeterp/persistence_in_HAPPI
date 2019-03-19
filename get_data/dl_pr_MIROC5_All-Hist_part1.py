325733a5ac3b8dea3678cb0ce98b41500d1168f9
peterpeterp <peterpfleiderer@gmx.de>
2019-03-19 19:06:45 +0100

    pr

---
+++ get_data/dl_pr_MIROC5_All-Hist_part1.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+import sys
+
+if (sys.version_info > (3, 0)):
+  from urllib.request import urlretrieve
+else:
+  from urllib import urlretrieve
+def hook(a,b,c): print(a*b,"/",c, "\r", end="")
+
+import os,sys,glob,time,collections,signal,gc
+
+os.chdir('/p/tmp/pepflei/HAPPI/')
+
+sys.path.append('/p/projects/ikiimp/HAPPI/HAPPI_Peter/persistence_in_HAPPI/')
+import __settings
+model_dict=__settings.model_dict
+
+model = 'MIROC5'
+scenario = 'All-Hist'
+var = 'pr'
+
+os.system('mkdir -p raw_data/'+model+'/'+scenario+'/'+var+'/tmp')
+os.chdir('raw_data/'+model+'/'+scenario+'/'+var)
+
+for run in ['run'+str(i).zfill(3) for i in range(1,51)]:
+    if os.path.isfile("pr_Aday_MIROC5_All-Hist_CMIP5-MMM-est1_v2-0_"+run+"_20060101-20161231.nc") == False:
+
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20060101-20061231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20060101-20061231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20060101-20061231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20070101-20071231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20070101-20071231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20070101-20071231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20080101-20081231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20080101-20081231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20080101-20081231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20090101-20091231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20090101-20091231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20090101-20091231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20100101-20101231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20100101-20101231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20100101-20101231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20110101-20111231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20110101-20111231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20110101-20111231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20120101-20121231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20120101-20121231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20120101-20121231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20130101-20131231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20130101-20131231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20130101-20131231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20140101-20141231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20140101-20141231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20140101-20141231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20150101-20151031.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20150101-20151031.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20150101-20151031.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20151101-20151231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20151101-20151231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20151101-20151231.nc", hook)
+        print('\n')
+        print("downloading: pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20160101-20161231.nc")
+        urlretrieve("http://portal.nersc.gov/cascade/data/rtrack.php?source=NERSCDisk&filename=MIROC/MIROC5/All-Hist/est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20160101-20161231.nc","tmp/pr_Aday_MIROC5_All-Hist_est1_v2-0_"+run+"_20160101-20161231.nc", hook)
+        print('\n')
+
+        os.system("cdo mergetime tmp/* pr_Aday_MIROC5_All-Hist_CMIP5-MMM-est1_v2-0_"+run+"_20060101-20161231.nc")
+        os.system("rm tmp/*")
---
+++ get_data/dl_pr_NorESM1_Plus15-Future.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import sys
+
+if (sys.version_info > (3, 0)):
+  from urllib.request import urlretrieve
+else:
+  from urllib import urlretrieve
+def hook(a,b,c): print(a*b,"/",c, "\r", end="")
+
+import os,sys,glob,time,collections,signal,gc
+
+os.chdir('/p/tmp/pepflei/HAPPI')
+
+sys.path.append('/p/projects/ikiimp/HAPPI/HAPPI_Peter/persistence_in_HAPPI/')
+import __settings
+model_dict=__settings.model_dict
+
+model = 'NorESM1'
+scenario = 'Plus15-Future'
+var = 'pr'
+
+os.system('mkdir -p raw_data/'+model+'/'+scenario+'/'+var)
+os.chdir('raw_data/'+model+'/'+scenario+'/'+var)
+
+for run in model_dict[model]['runs'][scenario]:
+    print("downloading: pr_Aday_NorESM1-HAPPI_Plus15-Future_CMIP5-MMM-est1_v2-0_"+run+"_21060101-21160630.nc")
+    urlretrieve("https://portal.nersc.gov/c15c/data/NCC/NorESM1-HAPPI/Plus15-Future/CMIP5-MMM-est1/v2-0/day/atmos/pr/"+run+"/pr_Aday_NorESM1-HAPPI_Plus15-Future_CMIP5-MMM-est1_v2-0_"+run+"_21060101-21160630.nc","pr_Aday_NorESM1-HAPPI_Plus15-Future_CMIP5-MMM-est1_v2-0_"+run+"_21060101-21160630.nc", hook)
+    print('\n')
