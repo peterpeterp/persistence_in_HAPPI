746201bf6b46e766c12f8731f7c917b9ccce0cc9
peterpeterp <peterpfleiderer@gmx.de>
2019-03-20 12:37:15 +0100

    do spi

--- analysis_add/add_61_SPI.r
+++ analysis_add/add_61_SPI.r
@@ -1,7 +1,6 @@
 library(ncdf4)
 library(SPEI)
 
-
 args = commandArgs(trailingOnly=TRUE)
 print(paste('infile',args[1]))
 print(paste('precip var name',args[2]))
@@ -18,29 +17,28 @@ pr<-ncvar_get(nc,args[2])
 lon<-ncvar_get(nc,'lon')
 lat<-ncvar_get(nc,'lat')
 time<-ncvar_get(nc,'time')
-'''
-this is weird
 
 spi=pr*NA
-for (x in 1:length(lon)){
+for (x in 1:20){ # length(lon)
 	cat(paste("--",x,proc.time()[3][[1]]))
 	for (y in 1:length(lat)){
-		spi[x,y,]<-spi(ts(pr[x,y,], freq=12, start=c(as.numeric(args[4]),1)), as.numeric(args[3]), ref.start=c(as.numeric(args[5]),1), ref.end=c(as.numeric(args[6]),12), na.rm = TRUE)$fitted
+		if (sum(is.finite(pr[x,y,]))>100){
+			spi[x,y,]<-spi(ts(pr[x,y,], freq=12, start=c(as.numeric(args[4]),1)), as.numeric(args[3]), ref.start=c(as.numeric(args[5]),1), ref.end=c(as.numeric(args[6]),12), na.rm = TRUE)$fitted
+			print(spi[x,y,])
+		}
 	}
-
-
-
-	londim <- ncdim_def(name="lon",units="deg east",vals=lon)
-	latdim <- ncdim_def(name="lat",units="deg north",vals=lat)
-	# adapt time stamp
-	timedim <- ncdim_def(name="time",units="days since 1860-1-1 00:00:00",vals=time,unlim=FALSE)
-
-	spivar <- ncvar_def(name="SPI",units="-",dim=list(londim,latdim,timedim),missval=-99999.9,longname=paste('SPI ',args[3],'months'))
-
-	nc_out <- nc_create( args[7], spivar )
-	ncvar_put( nc_out, vals=spi)
-
-	nc_close(nc_out)
-
 }
-'''
+
+
+londim <- ncdim_def(name="lon",units="deg east",vals=lon)
+latdim <- ncdim_def(name="lat",units="deg north",vals=lat)
+# adapt time stamp
+timedim <- ncdim_def(name="time",units="days since 1850-1-1 00:00:00",vals=time,unlim=FALSE)
+
+spivar <- ncvar_def(name="SPI",units="-",dim=list(londim,latdim,timedim),missval=-99999.9,longname=paste('SPI ',args[3],'months'))
+
+print(spivar)
+nc_out <- nc_create( args[7], spivar)
+ncvar_put( nc_out, vals=spi)
+
+nc_close(nc_out)
