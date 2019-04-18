4d5485553d5a205a24df2a1d574f307cf504ae4e
peterpeterp <peterpfleiderer@gmx.de>
2019-04-18 18:27:25 +0200

    redo eobs

---
+++ analysis_drive/obs_correlation_EOBS.py
@@ -0,0 +1,212 @@
+import os,sys,glob,time,collections,signal,gc
+import numpy as np
+from netCDF4 import Dataset,num2date
+import random as random
+import dimarray as da
+import subprocess as sub
+import scipy.stats as stats
+from scipy import signal
+import scipy
+
+model='EOBS'
+scenario='All-Hist'
+
+
+os.chdir('/p/projects/ikiimp/HAPPI/HAPPI_Peter')
+corWith_dict = {
+	'SPI3':{'file':'data/rr_0p5_1950-2018_mon_SPI3.nc','corWith_start':1950,'corWith_end':2018},
+	'EKE':{'file':'data/ERA-Interim_eke_1979-2017_850hPa_mon_EOBSgrid.nc','corWith_start':1979,'corWith_end':2017}
+}
+
+working_path='data/'+model+'/'
+
+for state,pers_file in zip(['dry-warm','warm','dry','5mm'], ['cpd_0.50deg_reg_merged_period_dry-warm.nc','tg_0.50deg_reg_merged_period_warm.nc','rr_0.50deg_reg_merged_period_dry.nc','rr_0.50deg_reg_merged_period_5mm.nc']):
+
+	data =da.read_nc(working_path+scenario+'/'+pers_file)
+	pers_start = 1950
+	pers_end = 2017
+
+	for corWith_name,corWith_detials in corWith_dict.items():
+		corWith_full = da.read_nc(corWith_detials['file'])['spi']
+
+		'''
+		carefully check time axis of both files
+		'''
+		corWith_start = corWith_detials['corWith_start']
+		corWith_end = corWith_detials['corWith_end']
+
+		index_shift = (pers_start - corWith_start) * 12
+		cut_start = corWith_start - pers_start
+		cut_end = (min(corWith_end,pers_end) - pers_start ) * 12
+
+		seasons={'MAM':0, 'JJA':1, 'SON':2, 'DJF':3}
+
+		cor = {}
+		for style in ['','_lagged','_season','_lagged_season','_mon','_mon_lagged']:
+			for stat in ['corrcoef','p-value']:
+				cor[stat+style]=da.DimArray(axes=[seasons.keys(),data.latitude,data.longitude],dims=['season','lat','lon'])
+		for stat in ['lr_intercept','lr_slope','lr_pvalue']:
+			cor[stat]=da.DimArray(axes=[seasons.keys(),data.latitude,data.longitude],dims=['season','lat','lon'])
+
+		statistics = {}
+		for xxx in [corWith_name,state,corWith_name+'_lagged']:
+			for stat in ['mean','10','25','33','50','66','75','90','100']:
+				statistics[stat+'_'+xxx]=da.DimArray(axes=[seasons.keys(),data.latitude,data.longitude],dims=['season','lat','lon'])
+		statistics['mean_of_10percLongest_'+state] = da.DimArray(axes=[seasons.keys(),data.latitude,data.longitude],dims=['season','lat','lon'])
+		statistics['mean_of_10percLongest_'+corWith_name] =  da.DimArray(axes=[seasons.keys(),data.latitude,data.longitude],dims=['season','lat','lon'])
+		statistics['mean_of_10percLongest_lagged_'+corWith_name] =  da.DimArray(axes=[seasons.keys(),data.latitude,data.longitude],dims=['season','lat','lon'])
+
+
+		for y in data.latitude:
+			for x in data.longitude:
+				pers_loc=data['period_length'][:,y,x].values
+				# print(np.nanpercentile(pers_loc,range(101)))
+				if pers_loc.sum() != 0:
+					print(y,x)
+
+					pre_index=data['period_monthly_index'][:,y,x]
+					avail_indices = np.where((pre_index>=cut_start) & (pre_index<=cut_end))[0]
+
+					pers_loc=data['period_length'][avail_indices,y,x].values
+					time_loc=data['period_midpoints'][avail_indices,y,x].values
+					monIndex_loc=data['period_monthly_index'][avail_indices,y,x].values
+					corWith_loc=corWith_full.ix[monIndex_loc+index_shift,:,:][:,y,x].values
+
+					tmp_lagged_index = monIndex_loc+index_shift-1
+					issue_months = np.where(tmp_lagged_index < 0)[0]
+					tmp_lagged_index[issue_months] = 0
+					tmp_tmp = corWith_full.ix[tmp_lagged_index,:,:][:,y,x].values
+					tmp_tmp[issue_months] = np.nan
+					corWith_loc_lagged = tmp_tmp
+
+					# mask all
+					mask = ~np.isnan(pers_loc) & ~np.isnan(time_loc) & ~np.isnan(corWith_loc) & ~np.isnan(corWith_loc_lagged)
+					time_loc=time_loc[mask]
+					if len(time_loc) > 1:
+						year_loc =  np.array([date.year for date in num2date(time_loc, units = "days since 1950-01-01 00:00")])
+						pers_loc=pers_loc[mask]
+						monIndex_loc=monIndex_loc[mask]
+						corWith_loc=corWith_loc[mask]
+						corWith_loc_lagged=corWith_loc_lagged[mask]
+						sea_loc=data['period_season'][:,y,x].values[mask]
+
+						for season_name,season_id in seasons.items():
+							valid_season = sea_loc==season_id
+							pers_loc_sea = np.array(pers_loc[valid_season], np.float)
+							if pers_loc_sea.shape[0]>10:
+								time_loc_sea = time_loc[valid_season]
+								year_loc_sea = year_loc[valid_season]
+								monIndex_loc_sea = monIndex_loc[valid_season]
+								corWith_loc_sea = corWith_loc[valid_season]
+								corWith_loc_lagged_sea = corWith_loc_lagged[valid_season]
+
+
+								################
+								# mean
+								################
+								statistics['mean_'+state][season_name,y,x] = np.nanmean(pers_loc_sea)
+								for qu in [10,25,33,50,66,75,90,100]:
+									statistics[str(qu)+'_'+state][season_name,y,x] = np.nanpercentile(pers_loc_sea,qu)
+
+								statistics['mean_'+corWith_name][season_name,y,x] = np.nanmean(corWith_loc_sea)
+								for qu in [10,25,33,50,66,75,90,100]:
+									statistics[str(qu)+'_'+corWith_name][season_name,y,x] = np.nanpercentile(corWith_loc_sea,qu)
+
+								statistics['mean_'+corWith_name+'_lagged'][season_name,y,x] = np.nanmean(corWith_loc_lagged_sea)
+								for qu in [10,25,33,50,66,75,90,100]:
+									statistics[str(qu)+'_'+corWith_name+'_lagged'][season_name,y,x] = np.nanpercentile(corWith_loc_lagged_sea,qu)
+
+
+								'''
+								detrend and normalize
+								detrending might be stupid in 10 year runs
+
+								mean eke/spi of 10% longest events
+
+								spi before event
+								'''
+
+								# # detrend
+								# slope, intercept, r_value, p_value, std_err = stats.linregress(time_loc_sea,pers_loc_sea)
+								# pers_loc_sea_detrend = pers_loc_sea-(intercept+slope*time_loc_sea)
+								# slope, intercept, r_value, p_value, std_err = stats.linregress(time_loc_sea,corWith_loc_sea)
+								# corWith_loc_sea_detrend = corWith_loc_sea-(intercept+slope*time_loc_sea)
+								# slope, intercept, r_value, p_value, std_err = stats.linregress(time_loc_sea,corWith_loc_lagged_sea)
+								# corWith_loc_lagged_sea_detrend = corWith_loc_lagged_sea-(intercept+slope*time_loc_sea)
+
+								# detrend
+								pers_loc_sea = scipy.signal.detrend(pers_loc_sea)
+								corWith_loc_sea = scipy.signal.detrend(corWith_loc_sea)
+								corWith_loc_lagged_sea = scipy.signal.detrend(corWith_loc_lagged_sea)
+
+
+								# normalize
+								pers_loc_sea_norm = (pers_loc_sea - pers_loc_sea.max()) / (pers_loc_sea.min() - pers_loc_sea.max())
+								corWith_loc_sea_norm = (corWith_loc_sea - corWith_loc_sea.max()) / (corWith_loc_sea.min() - corWith_loc_sea.max())
+								corWith_loc_lagged_sea_norm = (corWith_loc_lagged_sea - corWith_loc_lagged_sea.max()) / (corWith_loc_lagged_sea.min() - corWith_loc_lagged_sea.max())
+
+								################
+								# correlation
+								################
+								cor['corrcoef'][season_name,y,x],cor['p-value'][season_name,y,x] = stats.pearsonr(pers_loc_sea_norm,corWith_loc_sea_norm)
+								cor['corrcoef_lagged'][season_name,y,x],cor['p-value_lagged'][season_name,y,x] = stats.pearsonr(pers_loc_sea_norm,corWith_loc_lagged_sea_norm)
+
+								################
+								# regression
+								################
+								slope, intercept, r_value, p_value, std_err = stats.linregress(pers_loc_sea_norm,corWith_loc_sea_norm)
+								cor['lr_slope'][season_name,y,x] = slope
+								cor['lr_intercept'][season_name,y,x] = intercept
+								cor['lr_pvalue'][season_name,y,x] = p_value
+
+								###################
+								# longest in season
+								###################
+								pers_loc_sea_, corWith_loc_sea_, corWith_loc_lagged_sea_ = np.array([]), np.array([]), np.array([])
+								for yr in sorted(set(year_loc_sea)):
+									indices_of_year = np.where(year_loc_sea==yr)[0]
+									corWith_loc_sea_ = np.append(corWith_loc_sea_,corWith_loc_sea_norm[indices_of_year][0])
+									corWith_loc_lagged_sea_ = np.append(corWith_loc_lagged_sea_,corWith_loc_lagged_sea_norm[indices_of_year][0])
+									pers_loc_sea_ = np.append(pers_loc_sea_,pers_loc_sea_norm[indices_of_year].max())
+
+								cor['corrcoef_season'][season_name,y,x],cor['p-value_season'][season_name,y,x] = stats.pearsonr(pers_loc_sea_,corWith_loc_sea_)
+								cor['corrcoef_lagged_season'][season_name,y,x],cor['p-value_lagged_season'][season_name,y,x] = stats.pearsonr(pers_loc_sea_,corWith_loc_lagged_sea_)
+
+
+								################
+								# longest in month
+								################
+								pers_loc_sea_, corWith_loc_sea_, corWith_loc_lagged_sea_ = np.array([]), np.array([]), np.array([])
+								for ind in sorted(set(monIndex_loc_sea)):
+									indices_of_mon = np.where(monIndex_loc_sea==ind)[0]
+									corWith_loc_sea_ = np.append(corWith_loc_sea_,corWith_loc_sea[indices_of_mon][0])
+									corWith_loc_lagged_sea_ = np.append(corWith_loc_lagged_sea_,corWith_loc_lagged_sea[indices_of_mon][0])
+									pers_loc_sea_ = np.append(pers_loc_sea_,pers_loc_sea[indices_of_mon].max())
+									# time_ = np.append(time_,time_loc_sea[indices_of_mon][np.argmax(pers_loc_sea[indices_of_mon])])
+
+								# normalize
+								pers_loc_sea_norm_ = (pers_loc_sea_ - pers_loc_sea_.max()) / (pers_loc_sea_.min() - pers_loc_sea_.max())
+								corWith_loc_sea_norm_ = (corWith_loc_sea_ - corWith_loc_sea_.max()) / (corWith_loc_sea_.min() - corWith_loc_sea_.max())
+								corWith_loc_lagged_sea_norm_ = (corWith_loc_lagged_sea_ - corWith_loc_lagged_sea_.max()) / (corWith_loc_lagged_sea_.min() - corWith_loc_lagged_sea_.max())
+
+								################
+								# correlation
+								################
+								cor['corrcoef_mon'][season_name,y,x],cor['p-value'][season_name,y,x] = stats.pearsonr(pers_loc_sea_norm_,corWith_loc_sea_norm_)
+								cor['corrcoef_mon_lagged'][season_name,y,x],cor['p-value_lagged'][season_name,y,x] = stats.pearsonr(pers_loc_sea_norm_,corWith_loc_lagged_sea_norm_)
+
+								###################
+								# longest in events
+								###################
+								longest_events = np.argwhere(pers_loc_sea >= np.percentile(pers_loc_sea,90))
+								statistics['mean_of_10percLongest_'+state][season_name,y,x] = np.nanmean(pers_loc_sea[longest_events])
+								statistics['mean_of_10percLongest_'+corWith_name][season_name,y,x] = np.nanmean(corWith_loc_sea[longest_events])
+								statistics['mean_of_10percLongest_lagged_'+corWith_name][season_name,y,x] = np.nanmean(corWith_loc_lagged_sea[longest_events])
+
+
+								gc.collect()
+
+		ds = da.Dataset(cor)
+		ds.write_nc(working_path+'/cor_'+corWith_name+'_'+'_'.join(['EOBS',state])+'.nc')
+
+		da.Dataset(statistics).write_nc(working_path+'/stats_'+corWith_name+'_'+'_'.join(['EOBS',state])+'.nc')
--- plot_paper/NH_summary_drive.py
+++ plot_paper/NH_summary_drive.py
@@ -13,7 +13,7 @@ import matplotlib.pylab as plt
 from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 import seaborn as sns
-sns.set_style("whitegrid")
+sns.set_style("white")
 import matplotlib.ticker as mticker
 
 sys.path.append('/Users/peterpfleiderer/Projects/allgemeine_scripte')
@@ -44,21 +44,33 @@ NH_regs={'ALA':{'color':'darkgreen','pos_off':(+10,+7),'summer':'JJA','winter':'
 
 		'MED':{'color':'gray','pos_off':(-15,-5),'summer':'JJA','winter':'DJF'},
 		'WAS':{'color':'darkcyan','pos_off':(-5,-1),'summer':'JJA','winter':'DJF'},
-		'mid-lat':{'edge':'darkgreen','color':'none','alpha':1,'pos':(-142,42),'xlabel':'Period length [days]','ylabel':'Exceedence probability [%]','title':'','summer':'JJA','winter':'DJF','scaling_factor':1.3}}
+		'mid-lat':{'edge':'darkgreen','color':'none','alpha':1,'pos':(-142,42),'title':'','summer':'JJA','winter':'DJF','scaling_factor':1.3}}
 
 all_regs=NH_regs.copy()
 
 polygons=srex.copy()
 polygons['mid-lat']={'points':[(-180,35),(180,35),(180,60),(-180,60)]}
 
-icon_dict = {'storm_track':get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/weather.png')}
+icon_dict = {
+	'storm_track':plt.imread(get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/storm.png')),
+	# 'water':plt.imread(get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/drop.png')),
+	# 'dry':plt.imread(get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/plumbing.png')),
+	# 'drought':plt.imread(get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/nature.png')),
+	'increase':plt.imread(get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/increase.png')),
+	'decrease':plt.imread(get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/decrease.png')),
+	'rain':plt.imread(get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/rain.png')),
+	# 'no_rain':plt.imread(get_sample_data('/Users/peterpfleiderer/Projects/Persistence/plots/icons/no_rain.png')),
+}
 
 # ---------------------------- changes
 def legend_plot(subax,arg1=None,arg2=None,arg3=None,arg4=None,arg5=None):
 	subax.axis('off')
 
 def axis_settings(subax,label=False,arg1=None,arg2=None,arg3=None,arg4=None,arg5=None):
-	# subax.axis('off')
+	subax.set_xlim(-2,2)
+	subax.set_ylim(-2,2)
+	subax.set_yticklabels([])
+	subax.set_xticklabels([])
 	return(subax)
 
 def imscatter(x, y, image, ax=None, zoom=1):
@@ -76,42 +88,177 @@ def imscatter(x, y, image, ax=None, zoom=1):
         ab = AnnotationBbox(im, (x0, y0), xycoords='data', frameon=False)
         artists.append(ax.add_artist(ab))
     ax.update_datalim(np.column_stack([x, y]))
-    ax.autoscale()
+    # ax.autoscale()
     return artists
 
 def distrs(subax,region,arg1=None,arg2=None,arg3=None,arg4=None,arg5=None):
 	print('________'+region+'________')
-	if region == 'CGI':
-		for state,details in arg1[region].items():
-			print(icon_dict[details['icon']])
-			imscatter(details['x'], details['y'], icon_dict[details['icon']], zoom=0.05, ax=subax)
+	patches,colors = [], []
+	for state,details in state_details.items():
+		x,y = details['x'],details['y']
+		pc = PatchCollection([matplotlib.patches.Polygon([(x-1.0,y-1.0),(x+1.0,y-1.0),(x+1.0,y+1.0),(x-1.0,y+1.0)])], color=details['color'], edgecolor="k", alpha=0.5, lw=0.5)
+		subax.add_collection(pc)
+		# pc = PatchCollection([matplotlib.patches.Polygon([(x-0.58,y-0.58),(x+0.58,y-0.58),(x+0.58,y+0.58),(x-0.58,y+0.58)])], color='white', edgecolor="w", alpha=1, lw=0.5)
+		# subax.add_collection(pc)
+
+		if region=='mid-lat':
+			subax.annotate(legend_dict[state],xy=(x*1.9,y*1.8),ha={-1:'left',1:'right'}[x], va='center', fontsize=8,fontweight='bold')
+
+	for state,icons in arg1[region].items():
+		drivers=icons['drive']
+		if len(drivers)>0:
+			for icon,xx,yy in zip(drivers,{1:[0],2:[-0.33,0.33],3:[-0.5,0,0.5]}[len(drivers)],{1:[0],2:[0,0],3:[-0.33,0.33,-0.33]}[len(drivers)]):
+				imscatter(state_details[state]['x']*1.5+xx, state_details[state]['y']*1.2+yy, icon_dict[icon], zoom=0.03 * (1/float(len(drivers)))**0.5, ax=subax)
+
+		if icons['change'] != 'none':
+			imscatter(state_details[state]['x']*0.7, state_details[state]['y']*0.9, icon_dict[icons['change']], zoom=0.025, ax=subax)
+
+	# for state,icons in arg1[region].items():
+	# 	drivers=icons['drive']
+	# 	if len(drivers)>0:
+	# 		for icon,xx,yy in zip(drivers,{1:[0],2:[-0.33,0.33],3:[-0.5,0,0.5]}[len(drivers)],{1:[0],2:[0,0],3:[-0.33,0.33,-0.33]}[len(drivers)]):
+	# 			imscatter(state_details[state]['x']*0.8+xx, state_details[state]['y']*1.2+yy, icon_dict[icon], zoom=0.025 * (1/float(len(drivers)))**0.5, ax=subax)
+	#
+	# 	if icons['change'] != 'none':
+	# 		imscatter(state_details[state]['x']*1.65, state_details[state]['y']*0.5, icon_dict[icons['change']], zoom=0.02, ax=subax)
+
+	subax.annotate(region, xy=(0.5, 0.5), xycoords='axes fraction', color='k', weight='bold', fontsize=8, ha='center', va='center', backgroundcolor='w')
+
 
 plt.rcParams["font.weight"] = "bold"
 plt.rcParams["axes.labelweight"] = "bold"
 
 legend_dict = {'warm':'warm','dry':'dry','dry-warm':'dry-warm','5mm':'rain'}
 
-arg1 = {'ALA':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'WNA':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'CNA':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'ENA':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'CGI':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		# 'CAM':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
+state_details = {
+	'warm':{'x':-1,'y':1,'color':'#FF3030'},
+	'dry':{'x':1,'y':1,'color':'#FF8C00'},
+	'dry-warm':{'x':-1,'y':-1,'color':'#BF3EFF'},
+	'5mm':{'x':1,'y':-1,'color':'#009ACD'},
+}
 
-		'NEU':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'CEU':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'CAS':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'NAS':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'TIB':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'EAS':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
+arg1 = {'ALA':{
+				'warm':{'drive':[],'change':'none'},
+				'dry':{'drive':['rain'],'change':'decrease'},
+				'dry-warm':{'drive':[],'change':'decrease'},
+				'5mm':{'drive':['rain'],'change':'increase'},
+				},
+		'WNA':{
+				'warm':{'drive':[],'change':'increase'},
+				'dry':{'drive':[],'change':'none'},
+				'dry-warm':{'drive':[],'change':'none'},
+				'5mm':{'drive':['rain'],'change':'decrease'},
+				},
+		'CNA':{
+				'warm':{'drive':['storm_track'],'change':'increase'},
+				'dry':{'drive':['storm_track'],'change':'increase'},
+				'dry-warm':{'drive':['storm_track'],'change':'increase'},
+				'5mm':{'drive':[],'change':'increase'},
+				},
+		'ENA':{
+				'warm':{'drive':['storm_track'],'change':'increase'},
+				'dry':{'drive':['storm_track'],'change':'increase'},
+				'dry-warm':{'drive':['storm_track'],'change':'increase'},
+				'5mm':{'drive':[],'change':'increase'},
+				},
+		'CGI':{
+				'warm':{'drive':[],'change':'none'},
+				'dry':{'drive':['rain'],'change':'decrease'},
+				'dry-warm':{'drive':[],'change':'none'},
+				'5mm':{'drive':['rain'],'change':'increase'},
+				},
+		'NEU':{
+				'warm':{'drive':['storm_track'],'change':'increase'},
+				'dry':{'drive':['storm_track'],'change':'increase'},
+				'dry-warm':{'drive':['storm_track'],'change':'increase'},
+				'5mm':{'drive':[],'change':'increase'},
+				},
+		'CEU':{
+				'warm':{'drive':['storm_track'],'change':'increase'},
+				'dry':{'drive':['storm_track'],'change':'increase'},
+				'dry-warm':{'drive':['storm_track'],'change':'increase'},
+				'5mm':{'drive':[],'change':'increase'},
+				},
+		'CAS':{
+				'warm':{'drive':['storm_track'],'change':'none'},
+				'dry':{'drive':[],'change':'none'},
+				'dry-warm':{'drive':['storm_track'],'change':'none'},
+				'5mm':{'drive':[],'change':'none'},
+				},
+		'NAS':{
+				'warm':{'drive':['storm_track'],'change':'increase'},
+				'dry':{'drive':['storm_track'],'change':'increase'},
+				'dry-warm':{'drive':['storm_track'],'change':'increase'},
+				'5mm':{'drive':['rain'],'change':'increase'},
+				},
+		'TIB':{
+				'warm':{'drive':[],'change':'increase'},
+				'dry':{'drive':[],'change':'none'},
+				'dry-warm':{'drive':['storm_track'],'change':'increase'},
+				'5mm':{'drive':[],'change':'none'},
+				},
+		'EAS':{
+				'warm':{'drive':[],'change':'none'},
+				'dry':{'drive':[],'change':'decrease'},
+				'dry-warm':{'drive':[],'change':'none'},
+				'5mm':{'drive':[],'change':'increase'},
+				},
 
-		'MED':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'WAS':{'warm':{'x':1,'y':1,'icon':'storm_track'}},
-		'mid-lat':{'warm':{'x':1,'y':1,'icon':'storm_track'}}
+		'MED':{
+				'warm':{'drive':[],'change':'none'},
+				'dry':{'drive':[],'change':'increase'},
+				'dry-warm':{'drive':[],'change':'none'},
+				'5mm':{'drive':[],'change':'none'},
+				},
+		'WAS':{
+				'warm':{'drive':[],'change':'none'},
+				'dry':{'drive':[],'change':'none'},
+				'dry-warm':{'drive':[],'change':'none'},
+				'5mm':{'drive':[],'change':'increase'},
+				},
+		'mid-lat':{
+				'warm':{'drive':['storm_track'],'change':'increase'},
+				'dry':{'drive':[],'change':'increase'},
+				'dry-warm':{'drive':[],'change':'increase'},
+				'5mm':{'drive':[],'change':'increase'},
+				},
 }
 
 
-fig,ax_map=srex_overview.srex_overview(distrs, axis_settings, polygons=polygons, reg_info=all_regs, x_ext=[-180,180], y_ext=[0,85], small_plot_size=0.08, legend_plot=legend_plot, legend_pos=[164,9], \
+fig,ax_map=srex_overview.srex_overview(distrs, axis_settings, polygons=polygons, reg_info=all_regs, x_ext=[-180,180], y_ext=[0,85], small_plot_size=0.08, legend_plot=legend_plot, legend_pos=[-160,20], \
 	arg1=arg1,
 	title=None)
+
+legax = fig.add_axes([0.01,0.01,0.3,0.2])
+legax.set_yticklabels([])
+legax.set_xticklabels([])
+x,y = 0,4
+for state,details in state_details.items():
+	pc = PatchCollection([matplotlib.patches.Polygon([(x-0.5,y-0.9),(x+0.5,y-0.9),(x+0.5,y+0.9),(x-0.5,y+0.9)])], color=details['color'], edgecolor="k", alpha=0.5, lw=0.5)
+	legax.add_collection(pc)
+	legax.annotate(legend_dict[state],xy=(1,y),ha='left', va='center', fontsize=8,fontweight='bold')
+	y-=2
+legax.annotate('Persistence',xy=(1,6.5),ha='left', va='center', fontsize=8,fontweight='bold')
+
+x,y = 5,3
+for icon_name,icon_realname in zip(['increase','decrease'],['increase','decrease']):
+	imscatter(x, y, icon_dict[icon_name], zoom=0.025, ax=legax)
+	legax.annotate(icon_realname,xy=(x+1,y),ha='left', va='center', fontsize=8,fontweight='bold')
+	y-=3
+legax.annotate('Change',xy=(x+1,6.5),ha='left', va='center', fontsize=8,fontweight='bold')
+
+x,y = 10,3
+for icon_name,icon_realname in zip(['storm_track','rain'],['storm tracks','water cycle']):
+	imscatter(x, y, icon_dict[icon_name], zoom=0.025, ax=legax)
+	legax.annotate(icon_realname,xy=(x+1,y),ha='left', va='center', fontsize=8,fontweight='bold')
+	y-=3
+legax.annotate('Driver',xy=(x+1,6.5),ha='left', va='center', fontsize=8,fontweight='bold')
+
+# legax.axhline(y=5.5,color='k')
+
+legax.set_xlim(-1,16)
+legax.set_ylim(-3,8)
+
+
+
 plt.savefig('plots/NH_summary_drive.png',dpi=600)
